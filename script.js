4cc103e641084da725db8e389eb301e358bb2144
procopio420 <lucas.p@gec.inatel.br>
2020-04-06 20:22:16 -0300

    requisitos obrigatorios-tentativa1

---
+++ script.js
@@ -0,0 +1,63 @@
+const input = document.getElementById('texto-tarefa');
+const btn = document.getElementById('criar-tarefa');
+const btnApagar = document.getElementById('apaga-tudo');
+const btnFinalizados = document.getElementById('remover-finalizados');
+const list = document.getElementById('lista-tarefas');
+const allBtn = document.getElementsByTagName('button');
+let items = document.getElementsByClassName('list-item');
+let finished = [];
+
+function selectItem() {
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].classList.remove('selected');
+  }
+  if (event.target.classList[1] !== 'selected') {
+    event.target.classList.add('selected');
+  }
+}
+
+function finishItem() {
+  let completed=false;
+  for(let i=0;i<event.target.classList.length;i++){
+    if(event.target.classList[i]==='completed') completed=true;
+  }
+  if(completed) event.target.classList.remove('completed');
+  else event.target.classList.add('completed');
+}
+
+btn.addEventListener('click', () => {
+  const item = document.createElement('li');
+  item.className = 'list-item';
+  item.innerHTML = input.value;
+  item.addEventListener('click', selectItem);
+  item.addEventListener('dblclick', finishItem);
+  input.value = '';
+  list.appendChild(item);
+});
+
+function getItems() {
+  items = document.getElementsByClassName('list-item');
+}
+setInterval(getItems, 1000);
+
+btnApagar.addEventListener('click', ()=>{
+  list.innerHTML='';
+});
+
+btnFinalizados.addEventListener('click', ()=>{
+  for(let i=0;i<items.length;i+=1){
+    for(let u=0;u<items[i].classList.length;u+=1){
+      if(items[i].classList[u]==='completed'){
+        items[i].remove();
+      }
+    }
+  }
+})
+
+function changeCursor(){
+  event.target.style.cursor='pointer';
+}
+
+for(let i=0;i<allBtn.length;i++){
+  allBtn[i].addEventListener('mouseover', changeCursor);
+}
