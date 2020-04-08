a1df86077f872576f951c85009490c8efb208e38
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-08 17:29:36 -0300

    Add function click buton add item to the list

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 window.onload = function(){
-
+   
+    const taskBox = document.querySelector('#texto-tarefa');
+    const buttonevent = document.querySelector('#criar-tarefa');
+    const list = document.querySelector('#lista-tarefas');
+     //function on click button create a new item to list
+    buttonevent.addEventListener('click',function(event){
+        let taskText = taskBox.value;
+        console.log("click done!");
+        console.log("taskText :", taskText);
+        let li = document.createElement('li');
+        list.appendChild(li).innerHTML = taskText;
+      })
 }
