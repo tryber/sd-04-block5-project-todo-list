0372d0e487a92db12e2a003f3331ce588c57df3c
ederusena <eder.sena@live.com>
2020-04-08 15:48:05 -0300

    Botao Cria tarefa, appendchild

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let btnAdicionaLI = document.getElementById("criar-tarefa");
+let textAdiciona = document.getElementById("texto-tarefa");
+let listaAdiciona = document.getElementById("lista-tarefas");
+
+
+btnAdicionaLI.addEventListener('click', () => {
+    let nodeLi = document.createElement("li");
+    let nodeText = document.createTextNode(textAdiciona.value);
+    nodeLi.appendChild(nodeText);
+    listaAdiciona.appendChild(nodeLi);
+
+});
