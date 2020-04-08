9e21ca6cbd9404feaea022424e0acf16352b4bae
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-04-08 17:18:07 -0300

    code climate

--- script.js
+++ script.js
@@ -9,38 +9,38 @@ const paraCima = document.getElementById('mover-cima');
 const paraBaixo = document.getElementById('mover-baixo');
 
 function moveUp() {
-  let bottomLi = document.querySelector('.selected');
-  let upperLi = bottomLi.previousSibling;
-  listaTarefas.insertBefore(bottomLi, upperLi)
+  const bottomLi = document.querySelector('.selected');
+  const upperLi = bottomLi.previousSibling;
+  listaTarefas.insertBefore(bottomLi, upperLi);
 }
 
 function moveDown() {
-  let upperLi = document.querySelector('.selected');
-  let bottomLi = upperLi.nextSibling;
-  listaTarefas.insertBefore(bottomLi, upperLi)
+  const upperLi = document.querySelector('.selected');
+  const bottomLi = upperLi.nextSibling;
+  listaTarefas.insertBefore(bottomLi, upperLi);
 }
 
 function selectItem(e) {
   if (document.querySelector('.selected')) {
-    document.querySelector('.selected').classList.remove('selected')
-    }
+    document.querySelector('.selected').classList.remove('selected');
+  }
   e.target.classList.add('selected');
 }
 
 function doneItem(e) {
   if (e.target.classList.contains('completed')) {
     e.target.classList.remove('completed');
-  }else {
+  } else {
     e.target.classList.add('completed');
   }
 }
 
 function salvadorDeTarefas() {
-  localStorage.lista = listaTarefas.innerHTML
+  localStorage.lista = listaTarefas.innerHTML;
 }
 
 function tarefaParaLista() {
-  let item = document.createElement('li');
+  const item = document.createElement('li');
   item.addEventListener('click', selectItem);
   item.addEventListener('dblclick', doneItem);
   item.innerHTML = inputTarefa.value;
@@ -49,21 +49,21 @@ function tarefaParaLista() {
 }
 
 function apagadorDeFinalizado() {
-  let finalizados = document.getElementsByClassName('completed');
+  const finalizados = document.getElementsByClassName('completed');
   while (finalizados[0]) {
     finalizados[0].parentNode.removeChild(finalizados[0]);
   }
 }
 
 window.onload = function () {
-  if (localStorage.lista){
+  if (localStorage.lista) {
     listaTarefas.innerHTML = localStorage.lista;
     const itensLista = document.getElementsByTagName('li');
     for (let i = 0; i < itensLista.length; i += 1) {
       itensLista[i].addEventListener('click', selectItem);
       itensLista[i].addEventListener('dblclick', doneItem);
-    };
-  };
+    }
+  }
 }
 
 criarTarefa.addEventListener('click', tarefaParaLista);
