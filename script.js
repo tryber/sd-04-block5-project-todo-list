86e66440a1b8055682ee7d746ae804f0b64269a9
ederusena <eder.sena@live.com>
2020-04-08 15:56:47 -0300

    botao

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-
 let btnAdicionaLI = document.getElementById("criar-tarefa");
 let textAdiciona = document.getElementById("texto-tarefa");
 let listaAdiciona = document.getElementById("lista-tarefas");
 
 
 btnAdicionaLI.addEventListener('click', () => {
+  if (textAdiciona.value) {
     let nodeLi = document.createElement("li");
     let nodeText = document.createTextNode(textAdiciona.value);
     nodeLi.appendChild(nodeText);
     listaAdiciona.appendChild(nodeLi);
-
+    textAdiciona.value = "";
+  }
 });
