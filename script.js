b4ddea3a4305325c779c655aa3b5b2d1271ec13c
Filipe de Castro <filipeyoga@gmail.com>
2020-04-08 17:59:19 -0300

    cursos pointer

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ const newTarefa = document.querySelector('#texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
 const lista = document.querySelector('#lista-tarefas');
 
-button.addEventListener('click', function(e) {
-  e.preventDefault;
-  let node = document.createElement("li");
+button.addEventListener ('click', function(e) {
+  e.preventDefault();
+  const node = document.createElement('li');
   let textnode = document.createTextNode(newTarefa.value);
-  console.log(textnode);
   node.appendChild(textnode);
   lista.appendChild(node);
   newTarefa.value = '';
-  });
+});
