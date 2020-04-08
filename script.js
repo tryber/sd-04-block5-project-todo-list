2853786a8a8c797d16ba65c39e98528ced4658b3
Filipe de Castro <filipeyoga@gmail.com>
2020-04-08 18:54:40 -0300

    buttons added

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const newTarefa = document.querySelector('#texto-tarefa');
-const button = document.querySelector('#criar-tarefa');
+const buttonAdd = document.querySelector('#criar-tarefa');
 const lista = document.querySelector('#lista-tarefas');
+const buttonDel = document.querySelector('#apaga-tudo');
+const buttonLimpa = document.querySelector('#remover-finalizados');
+const elements = document.getElementsByClassName('completed');
 
-button.addEventListener ('click', function(e) {
+buttonAdd.addEventListener('click', function (e) {
   e.preventDefault();
   const node = document.createElement('li');
-  let textnode = document.createTextNode(newTarefa.value);
+  const textnode = document.createTextNode(newTarefa.value);
   node.appendChild(textnode);
   lista.appendChild(node);
   newTarefa.value = '';
 });
 
-lista.addEventListener ('click', function(e) {
-  e.target.classList.add('cinza');
+lista.addEventListener('click', function (e) {
+  e.target.classList.toggle('cinza');
 });
+
+lista.addEventListener('click', function (e) {
+  e.target.classList.toggle('completed');
+});
+
+buttonDel.addEventListener('click', function () {
+  lista.innerHTML = '';
+});
+
+function removeElementsByClass() {
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
+
+buttonLimpa.addEventListener('click', removeElementsByClass);
