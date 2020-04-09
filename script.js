a51bc89775226ce2da7a4eae3076d1551dcc9d60
Diego Matheus Campos <diegodiko05@gmail.com>
2020-04-08 23:06:58 -0300

    Adicionando cursor pointer nos ponteiros

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const ol = document.querySelector('#lista-tarefas');
 let input = document.querySelector('#texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
+
 console.log(button)
 
 function addTask() {
-  let li = document.createElement("li");
+  if(input.value != ""){
+    let li = document.createElement("li");
   li.appendChild(document.createTextNode(input.value));
   ol.appendChild(li);
   input.value = ""
+  }
 }
 button.addEventListener('click', addTask)
