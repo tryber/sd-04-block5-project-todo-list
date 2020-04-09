e746fb5197a44d3193cac83b6d3dfc5130487329
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-09 12:59:16 -0300

    add function color in gray selected item of the list

--- script.js
+++ script.js
@@ -6,11 +6,25 @@ window.onload = function(){
      //function on click button create a new item to list
     buttonevent.addEventListener('click',function(event){
         let taskText = taskBox.value;
-        console.log("click done!");
-        console.log("taskText :", taskText);
         let li = document.createElement('li');
+        li.classList.add("list");
         list.appendChild(li).innerHTML = taskText;
         taskBox.value = "";
       })
+//function to modify items of the list: put rgb(128,128,128)
+  document.body.addEventListener( 'click', function ( event ) {
+  console.log("event.target :",event.target);
+  if( event.target.className == 'list') {
+    console.log("done");
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+  };
+} );
+//temp 
+
+//temp end
 }
+
+
+
+
 
