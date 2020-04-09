8e4ba4a0be8232264ae60f18f3f93f2ae7089441
Diego Matheus Campos <diegodiko05@gmail.com>
2020-04-08 23:57:18 -0300

    Adicionando background collor em alguma li selecionada

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ let input = document.querySelector('#texto-tarefa');
 const button1 = document.querySelector('#criar-tarefa');
 const button2 = document.querySelector('#apaga-tudo');
 
-
-// Adicionando função de add tarefas no button
+// Adicionando função de add tarefas no button1
 function addTask() {
   if(input.value != ""){
-    let li = document.createElement("li");
+  let li = document.createElement("li");
   li.appendChild(document.createTextNode(input.value));
   ol.appendChild(li);
   input.value = ""
@@ -15,10 +14,15 @@ function addTask() {
 }
 button1.addEventListener('click', addTask)
 
-// Função apaga tudo
+// Função apaga tudo no button2
 function clear (){
   while (ol.firstChild) {
     ol.firstChild.remove();
-}
+  }
 }
 button2.addEventListener('click', clear)
+
+// Funda cinza para as tarefas selecionadas
+ol.addEventListener('click', function (e){
+  e.target.classList.toggle('gray');
+})
