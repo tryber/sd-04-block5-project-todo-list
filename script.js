4b95a90646d5c498e6a8746cc43f6277a80e077f
Diego Matheus Campos <diegodiko05@gmail.com>
2020-04-09 00:25:42 -0300

    Botao com a funçao remover finalizadas, e fim dos requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 const ol = document.querySelector('#lista-tarefas');
-let input = document.querySelector('#texto-tarefa');
+const input = document.querySelector('#texto-tarefa');
 const button1 = document.querySelector('#criar-tarefa');
 const button2 = document.querySelector('#apaga-tudo');
+const button3 = document.querySelector('#remover-finalizados');
 
 // Adicionando função de add tarefas no button1
 function addTask() {
-  if(input.value != ""){
-  let li = document.createElement("li");
-  li.appendChild(document.createTextNode(input.value));
-  ol.appendChild(li);
-  input.value = ""
+  if (input.value !== '') {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ol.appendChild(li);
+    input.value = '';
   }
 }
-button1.addEventListener('click', addTask)
+button1.addEventListener('click', addTask);
 
 // Função apaga tudo no button2
-function clear (){
+function clear() {
   while (ol.firstChild) {
     ol.firstChild.remove();
   }
 }
-button2.addEventListener('click', clear)
+button2.addEventListener('click', clear);
 
-// Funda cinza para as tarefas selecionadas
+// Fundo cinza para as tarefas selecionadas
 ol.addEventListener('click', function (e) {
   e.target.classList.toggle('gray');
-})
+});
 
-// Adicionanda a classe para as tarifas completadas que riscam
+// Adicionanda a classe para as tarifas completadas
 ol.addEventListener('dblclick', function (e) {
   e.target.classList.toggle('completed');
+});
+
+// Função para remover somentes os itens finalizados da lista:
+button3.addEventListener('click', function () {
+  const tasks = document.querySelectorAll('li');
+  for (let i = 0; i < tasks.length; i += 1) {
+    if (tasks[i].classList.contains('completed')) {
+      ol.removeChild(tasks[i]);
+    }
+  }
 });
