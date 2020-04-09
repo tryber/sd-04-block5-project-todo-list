ed8eabd5fd0c0b88345c12f9ac429595a65adc9f
Diego Matheus Campos <diegodiko05@gmail.com>
2020-04-08 22:53:07 -0300

    Adicionando botao com a funçao de adicionar tarefas

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const listaTarefas = document.querySelector('#lista-tarefas')
-console.log(listaTarefas)
+const ol = document.querySelector('#lista-tarefas');
+const input = document.querySelector('#texto-tarefa');
+const button = document.querySelector('#criar-tarefa');
+console.log(button)
+
+function addTask() {
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ol.appendChild(li);
+}
+button.addEventListener('click', addTask)
