7f7251121f240904760d7d6f2cb2c741322be2af
ederusena <eder.sena@live.com>
2020-04-08 23:17:46 -0300

    Acertos CSS

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-let btnAdicionaLI = document.getElementById('criar-tarefa');
-let textAdiciona = document.getElementById('texto-tarefa');
-let listaAdiciona = document.getElementById('lista-tarefas');
+const btnAdicionaLI = document.getElementById('criar-tarefa');
+const textAdiciona = document.getElementById('texto-tarefa');
+const listaAdiciona = document.getElementById('lista-tarefas');
 
 btnAdicionaLI.addEventListener('click', () => {
   if (textAdiciona.value) {
-    let nodeLi = document.createElement('li');
-    let nodeText = document.createTextNode(textAdiciona.value);
+    const nodeLi = document.createElement('li');
+    const nodeText = document.createTextNode(textAdiciona.value);
     nodeLi.appendChild(nodeText);
-    nodeLi.setAttribute('id','lista-li');
+    nodeLi.setAttribute('id', 'lista-li');
     listaAdiciona.appendChild(nodeLi);
     textAdiciona.value = '';
   }
 });
 
 listaAdiciona.addEventListener('click', (event) => {
-    if ( document.querySelector( '.selected' ) !== null ) {
-        document.querySelector( '.selected' ).classList.remove( 'selected' );
-      }
-      event.target.classList.add( 'selected' );
-  });
+  if (document.querySelector('.selected') !== null) {
+    document.querySelector('.selected').classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+});
 
-  listaAdiciona.addEventListener('ondblclick', (event) => {
-    if (document.querySelector('.selected') !== null) {
-        document.querySelector('.selected').classList.remove('selected');
-      }
-      event.target.classList.add('selected');
-  });
+listaAdiciona.addEventListener('ondblclick', (event) => {
+  if (document.querySelector('.selected') !== null) {
+    document.querySelector('.selected').classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+});
 
 
 listaAdiciona.addEventListener('dblclick', (event) => {
-    if (event.target.classList.contains('completed')) {
-      event.target.classList.remove('completed');
-    } else {
-      event.target.classList.add('completed');
-    }
-  });
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+});
