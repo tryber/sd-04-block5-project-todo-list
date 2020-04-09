72fca804781cc6e10ec4189ab838e645e6da2251
joaosoutto <joaosoutto92@gmail.com>
2020-04-09 11:49:48 -0300

    adicionando a função de adicionar item na lista

---
+++ script.js
@@ -0,0 +1,29 @@
+//  BOTÃO LIMPAR TUDO
+const clearListButton = document.getElementById('apaga-tudo');
+
+//  LISTA DAS TAREFAS
+const taskList = document.getElementById('lista-tarefas');
+
+//  TAREFA A SER INCLUIDA NA LISTA
+const taskToInput = document.getElementById('texto-tarefa');
+
+//
+const addTaskButton = document.getElementById('criar-tarefa');
+
+
+//  FUNÇÃO ADICIONAR TASk-----------------------------------------------
+function newTask() {
+    let li = document.createElement('li');
+    let inputValue = document.getElementById('texto-tarefa').value;
+    let text = document.createTextNode(inputValue);
+    li.appendChild(text);
+    if (inputValue === '') {
+        alert ('You must especify a task!')
+    }
+    else {
+        document.getElementById('lista-tarefas').appendChild(li);
+    }
+    document.getElementById('texto-tarefa').value = '';
+}
+addTaskButton.addEventListener('click', newTask);
+//  ----------------------------------------------------------------------
