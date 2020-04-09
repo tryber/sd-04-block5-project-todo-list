0ee9006ee6e567d9df610d6abd093f3f2001e1f4
orlandomessias <programadorlando@gmail.com>
2020-04-09 00:20:58 -0300

    set background color to the selected item

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let taskText = document.getElementById('texto-tarefa');
-let btnAdd = document.getElementById('criar-tarefa');
-let taskList = document.getElementById('lista-tarefas');
+const taskText = document.getElementById('texto-tarefa');
+const btnAdd = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+let listItem = '';
 let elem = null;
 let txt = null;
+
+function removeClass() {
+    for (let i = 0; i < listItem.length; i += 1) {
+      listItem[i].classList.remove('selected');
+    }
+}
+
+function addClickEvent(elem) {
+    elem.addEventListener('click', () => {  
+      removeClass();
+      elem.classList.add('selected');
+    });
+}
 
 btnAdd.addEventListener('click', () => {
   elem = document.createElement('li');
@@ -10,4 +24,6 @@ btnAdd.addEventListener('click', () => {
   elem.appendChild(txt);
   taskList.appendChild(elem);
   taskText.value = '';
+  listItem = document.querySelectorAll('ol li');
+  addClickEvent(elem);
 });
