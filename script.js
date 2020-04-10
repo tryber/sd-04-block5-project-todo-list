36ef2953605c858a62f14157175589f775a52d88
Douglas Costa <douglaza@gmail.com>
2020-04-10 15:43:36 -0300

    maybe 9/11 requirements done

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-function incluirItem() { //https://codare.aurelio.net/2006/12/20/javascript-construindo-listas-dinamicamente/
-  let tarefaTexto = document.getElementById("texto-tarefa");
-  let can = document.getElementById("lista-tarefas");
-  let li = document.createElement("li");
+const can = document.getElementById('lista-tarefas');
+
+function incluirItem() { // https://codare.aurelio.net/2006/12/20/javascript-construindo-listas-dinamicamente/
+  const tarefaTexto = document.getElementById('texto-tarefa');
+  const li = document.createElement('li');
+  li.className = 'cible';
   li.innerHTML = tarefaTexto.value.toUpperCase();
   can.appendChild(li);
-  document.getElementById("texto-tarefa").value = '';
+  document.getElementById('texto-tarefa').value = '';
 }
 
 function apagarLista() {
-  let can = document.getElementById("lista-tarefas");
-  let linhas = can.childNodes;
-  for (let i = linhas.length - 1; i >= 0; i--) {
-    if (linhas[i].tagName == 'LI') {
+  const linhas = can.childNodes;
+  for (let i = linhas.length - 1; i >= 0; i -= 1) {
+    if (linhas[i].tagName === 'LI') {
       can.removeChild(linhas[i]);
     }
   }
 }
+
+function changeBkg(e) {
+  e.target.classList.toggle('selected');
+}
+can.addEventListener('click', changeBkg);
+
+function completed(e) {
+  e.target.classList.toggle('tachado');
+}
+can.addEventListener('dblclick', completed);
