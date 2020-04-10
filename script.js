0f05de84e3bed6f447ca9d9ec48e8f8ce6cc0a90
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-04-09 21:45:32 -0300

    implementado criação de tarefas e limpeza da caixa de input

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+const buttonCriarTarefa = document.getElementById('criar-tarefa');
+const ol = document.getElementById('lista-tarefas');
+function criarTarefa() {
+  const li = document.createElement('li');
+  const inputTarefa = document.getElementById('texto-tarefa');
+  li.innerHTML = inputTarefa.value;
+  ol.appendChild(li);
+  inputTarefa.value = '';
+}
+buttonCriarTarefa.addEventListener('click', function () {
+  criarTarefa();
+});
