89385d6bc3089d7ecbeaa3919d3de2c51f480b9c
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-10 00:16:46 -0300

    Configurado  pointer para cadli.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 window.onload = function () {
-  let textoTarefa = document.getElementById('texto-tarefa'); // Manipula a caixa de texto.
-  let btnAdiciona = document.getElementById('criar-tarefa'); // Manipula o botão que adiciona na lista.
-  let lista = document.getElementById('lista-tarefas'); // Manipula a lista de tarefas.
+  const textoTarefa = document.getElementById('texto-tarefa'); // Manipula a caixa de texto.
+  const btnAdiciona = document.getElementById('criar-tarefa'); // Manipula o botão que adiciona na lista.
+  const lista = document.getElementById('lista-tarefas'); // Manipula a lista de tarefas.
 
   btnAdiciona.addEventListener('click', function () {
-    let item = document.createElement('li');
+    const item = document.createElement('li');
     item.innerHTML = textoTarefa.value;
     lista.appendChild(item);
     textoTarefa.value = '';
   });
-}
+};
