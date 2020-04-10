228069e99e51dd81b5d8ba9bbfe9ff3d62530101
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-10 02:01:01 -0300

    Adiconado botão com evento que apaga a lista inteira.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 window.onload = function () {
   const textoTarefa = document.getElementById('texto-tarefa'); // Manipula a caixa de texto.
   const btnAdiciona = document.getElementById('criar-tarefa'); // Manipula o botão que adiciona na lista.
+  const btnLimpa = document.getElementById('apaga-tudo'); // Manipula o botão que apaga a lista.
   const lista = document.getElementById('lista-tarefas'); // Manipula a lista de tarefas.
 
-  function alteraFundoItem() {
+  function selecionaItem() {
     event.target.style.backgroundColor = 'rgb(128,128,128)';
   }
 
@@ -18,12 +19,23 @@ window.onload = function () {
     }
   }
 
+  function apagaLista(number) {
+    for (let n = 0; n < number; n += 1) {
+      lista.removeChild(lista.childNodes[0]);
+    }
+  }
+
   btnAdiciona.addEventListener('click', function () {
     const item = document.createElement('li');
     item.innerHTML = textoTarefa.value;
-    item.addEventListener('click', alteraFundoItem);
+    item.addEventListener('click', selecionaItem);
     item.addEventListener('dblclick', marcaItem);
     lista.appendChild(item);
     textoTarefa.value = '';
+  });
+
+  btnLimpa.addEventListener('click', function () {
+    let comprimentoLista = lista.childNodes.length;
+    apagaLista(comprimentoLista);
   });
 };
