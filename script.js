f8a08c5216d81b1c4a90b2e567aeb8d45be29b2e
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-10 00:26:37 -0300

    Configurado evento que altera cor de fundo para cada item que recebe um clique.

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ window.onload = function () {
   const btnAdiciona = document.getElementById('criar-tarefa'); // Manipula o botão que adiciona na lista.
   const lista = document.getElementById('lista-tarefas'); // Manipula a lista de tarefas.
 
+  function alteraFundoItem(event) {
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+  }
+
   btnAdiciona.addEventListener('click', function () {
     const item = document.createElement('li');
     item.innerHTML = textoTarefa.value;
+    item.addEventListener('click', alteraFundoItem)
     lista.appendChild(item);
     textoTarefa.value = '';
   });
