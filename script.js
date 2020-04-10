ca52b10d15f3614d0639533d823b1331e74aaf50
AliceAAtalla <aliceatalla36@gmail.com>
2020-04-10 13:52:23 -0300

    Requisito 8 - JS

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-window.onload
+window.onload = function() {
+  backgroundList,
+}
+
+let list
+
+list = document.getElementById("lista-tarefas");
+
+list.addEventListener("click", function backgroundList() {
+  list.style.backgroundColor = "rgb(128,128,128)";
+});
