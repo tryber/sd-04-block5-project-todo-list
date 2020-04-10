5151e202efbeef48419b6ffc7611f14a8210b372
Douglas Costa <douglaza@gmail.com>
2020-04-10 16:28:06 -0300

    removing .toUpperCase commit

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const can = document.getElementById('lista-tarefas');
 function incluirItem() { // https://codare.aurelio.net/2006/12/20/javascript-construindo-listas-dinamicamente/
   const tarefaTexto = document.getElementById('texto-tarefa');
   const li = document.createElement('li');
-  li.className = 'cible';
-  li.innerHTML = tarefaTexto.value.toUpperCase();
+  // li.className = 'cible';
+  // li.innerHTML = tarefaTexto.value.toUpperCase();
+  li.innerHTML = tarefaTexto.value;
   can.appendChild(li);
   document.getElementById('texto-tarefa').value = '';
 }
@@ -18,12 +19,21 @@ function apagarLista() {
   }
 }
 
+function excluirItem() {
+  const linhas = can.childNodes;
+  for (let i = linhas.length - 1; i >= 0; i -= 1) {
+    if (linhas[i].className === 'completed') {
+      can.removeChild(linhas[i]);
+    }
+  }
+}
+
 function changeBkg(e) {
   e.target.classList.toggle('selected');
 }
 can.addEventListener('click', changeBkg);
 
 function completed(e) {
-  e.target.classList.toggle('tachado');
+  e.target.classList.toggle('completed');
 }
 can.addEventListener('dblclick', completed);
