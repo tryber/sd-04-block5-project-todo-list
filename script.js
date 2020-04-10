0c8e046f9d3c522daeb1610bb3b7256532f5a4a7
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-10 01:18:34 -0300

    Configurado evento que altera o estado do item quando clicaco duas vezes, um para marcá-lo como feito, outro para desfazer a ação anterior.

--- script.js
+++ script.js
@@ -3,14 +3,24 @@ window.onload = function () {
   const btnAdiciona = document.getElementById('criar-tarefa'); // Manipula o botão que adiciona na lista.
   const lista = document.getElementById('lista-tarefas'); // Manipula a lista de tarefas.
 
-  function alteraFundoItem(event) {
+  function alteraFundoItem() {
     event.target.style.backgroundColor = 'rgb(128,128,128)';
+  }
+
+  function marcaItem() {
+    let indentifier = event.target.style.textDecoration;
+    if(indentifier === 'line-through') {
+      event.target.style.textDecoration = 'none';
+    } else {
+      event.target.style.textDecoration = 'line-through';
+    }
   }
 
   btnAdiciona.addEventListener('click', function () {
     const item = document.createElement('li');
     item.innerHTML = textoTarefa.value;
-    item.addEventListener('click', alteraFundoItem)
+    item.addEventListener('click', alteraFundoItem);
+    item.addEventListener('dblclick', marcaItem);
     lista.appendChild(item);
     textoTarefa.value = '';
   });
