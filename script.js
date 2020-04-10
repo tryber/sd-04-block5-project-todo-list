7463f63109b8b9ee416e1a2a67b28309e18ce4f9
Ana Higo <anahigo.arq@gmail.com>
2020-04-09 21:02:32 -0300

    tentativa 01 - html

---
+++ script.js
@@ -0,0 +1,8 @@
+/*Ana 
+todos esses requisitos dependem de eventos ("addEventListener") e de propriedades css 
+quando você não souber o nome do evento ou da propriedade, joga o que tá pedindo no google 
+por exemplo 
+requisito 9 - "ao clicar duas vezes em um item..." joga no google : "evento javascript clicar duas vezes" 
+"item devera ser riscado".. joga no google: "propriedade css riscar elemento" 
+http://www.javascripter.net/faq/stylesc.htm
+https://www.w3schools.com/cssref/pr_class_cursor.asp*/
