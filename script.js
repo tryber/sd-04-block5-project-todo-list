3f2101ead85cd436dd113b35d38ff4809a3e5531
AliceAAtalla <aliceatalla36@gmail.com>
2020-04-11 17:04:26 -0300

    Adicionando botoes no html

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-let ol, list, tarefa, textValue, listOl, btnTarefa;
+let ol, item, createdLi, textValue, btnTarefa, inpText, paragraph;
 
-ol = document.querySelector("ol")
+ol = document.querySelector("#lista-tarefas");
 btnTarefa = document.getElementById("criar-tarefa");
 inpText = document.getElementById("texto-tarefa");
-listOl = document.getElementById("lista-tarefas");
 textValue = document.createTextNode(inpText.value);
-tarefa = document.createElement("li");
+createdLi = document.createElement("li");
+item = document.getElementsByTagName("li");
+paragraph = document.createElement("p");
+clearAll = document.getElementById("apaga-tudo");
 
-btnTarefa.addEventListener("click", function newElement() {
-  tarefa.className = "task";
-  ol.appendChild(tarefa);
-  tarefa.innerHTML = inpText.value;
-  inpText.value = "";
+btnTarefa.addEventListener("click", function () {
+  if (inpText.value === "") {
+    alert("Nenhuma tarefa adicionada");
+  } else {
+    for (let i = 0; i < 10; i += 1) {
+      createdLi.className = "task";
+      ol.appendChild(createdLi);
+      item[i] = createdLi.appendChild(paragraph);
+      paragraph.insertAdjacentHTML("afterbegin", inpText.value);
+      inpText.focus();//deixar o cursor na caixa de texto apos adicionar
+      inpText.value = "";
+    }
+  }
 });
 
-list.addEventListener("click", function backgroundList() {
-  list.style.backgroundColor = "rgb(128,128,128)";
+ for (let i = 0; i < item.length; i += 1) {
+ item[i].addEventListener("click", function () {
+   item[i].style.backgroundColor = "rgb(128, 128, 128)";
+  })
+ };
+
+clearAll.addEventListener("click", function () {
+  for (let i = 0; i < number; i +=1){
+    item.innerHTML = "";
+  }
 });
