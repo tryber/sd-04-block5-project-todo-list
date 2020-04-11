b313014dd6c5e3125be8fbe54878ec43c95cecf1
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-04-11 18:19:03 -0300

    steps9,10,11

--- script.js
+++ script.js
@@ -14,23 +14,46 @@ window.onload = function () {
     document.getElementById('texto-tarefa').value = '';
   }
 
-  document.addEventListener("mouseover", function (event) {
-    if (event.target && event.target.nodeName == "BUTTON") {
-      event.target.style.cursor = 'pointer';
-    }
-  });
+  // document.addEventListener("mouseover", function (event) {
+  //   if (event.target && event.target.nodeName == "BUTTON") {
+  //     event.target.style.cursor = 'pointer';
+  //   }
+  // });
 
-  document.getElementById("lista-tarefas").addEventListener("mouseover", function (event) {
-    if (event.target && event.target.nodeName == "LI") {
-      event.target.style.cursor = 'pointer';
-    }
-  });
+  // document.getElementById("lista-tarefas").addEventListener("mouseover", function (event) {
+  //   if (event.target && event.target.nodeName == "LI") {
+  //     event.target.style.cursor = 'pointer';
+  //   }
+  // });
 
   document.getElementById("lista-tarefas").addEventListener("click", function (event) {
     if (event.target && event.target.nodeName == "LI") {
       event.target.style.backgroundColor = "rgb(128, 128, 128)";
     }
   });
+
+  document.getElementById("lista-tarefas").addEventListener("dblclick", function (event) {
+    if (event.target && event.target.nodeName == "LI") {
+      if (event.target.className == "completed") {
+        event.target.classList.remove('completed')
+      } else event.target.classList.add('completed');
+    }
+  });
+
+  document.getElementById('apaga-tudo').addEventListener('click', limparTarefas);
+
+  function limparTarefas() {
+    document.getElementById("lista-tarefas").innerHTML = '';
+  }
+
+  document.getElementById('remover-finalizados').addEventListener('click', limparTarefasConcluidas);
+
+  function limparTarefasConcluidas() {
+    let tarefasCompletadas = document.getElementsByClassName("completed");
+    for (let i = 0; i < tarefasCompletadas.length; i+=1){
+      document.getElementById("lista-tarefas").removeChild(tarefasCompletadas[i]);      
+    }
+  }
 
 
 }
