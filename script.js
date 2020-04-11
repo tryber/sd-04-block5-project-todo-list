fd29589783eaec0c3ed24c586b1ed567a9a9109c
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-10 22:00:33 -0300

    Adicionado botão com evento que salva tarefas, e melhorada a forma de chamar as funções.

--- script.js
+++ script.js
@@ -2,9 +2,31 @@ const textoTarefa = document.getElementById('texto-tarefa'); // Manipula a caixa
 const btnAdiciona = document.getElementById('criar-tarefa'); // Manipula o botão que adiciona na lista.
 const btnLimpa = document.getElementById('apaga-tudo'); // Manipula o botão que apaga a lista.
 const btnLimpaConcluidas = document.getElementById('remover-finalizados'); // Manipula o botão que limpa itens concluídos.
+const btnSalvaTarefas = document.getElementById('salvar-tarefas');
 const lista = document.getElementById('lista-tarefas'); // Manipula a lista de tarefas.
 
+function storeExists() { // Verifica so o navegador tem suporte a Storage.
+  return typeof Storage !== 'undefined';
+}
+
+function salvaListaNoStorage() { // Salva os itens da lista no LocalStorage.
+  if(storeExists()) {
+    const itens = lista.childNodes;
+    for (let i = 0; i < itens.length; i += 1) {
+      localStorage.setItem(`item${i}`, `${itens[i].innerText}`);
+    }
+  } else {
+    alert('Seu navegador não tem suporte a Local Storage!');
+  }
+}
+
 function selecionaItem() { // Altera o fundo do item clicado.
+  const itens = document.querySelectorAll('li');
+  for (let i = 0; i < itens.length; i += 1) {
+    if (itens[i].style.backgroundColor === 'rgb(128, 128, 128)') {
+      itens[i].style.backgroundColor = 'white';
+    }
+  }
   event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
 
@@ -24,14 +46,27 @@ function apagaLista(number) { // Apaga todos os item da lista.
     lista.removeChild(lista.childNodes[0]);
   }
 }
-function eventBtnAdiciona() { // Evento para o botão que adiciona itens.
-  btnAdiciona.addEventListener('click', function () {
-    const item = document.createElement('li');
+
+function craiItem(value) {
+  const item = document.createElement('li');
+  if(!value == null) {
+    item.innerHTML = localStorage.getItem(value);
+    item.addEventListener('click', selecionaItem);
+    item.addEventListener('dblclick', marcaItem);
+    lista.appendChild(item);
+    textoTarefa.value = '';
+  } else {
     item.innerHTML = textoTarefa.value;
     item.addEventListener('click', selecionaItem);
     item.addEventListener('dblclick', marcaItem);
     lista.appendChild(item);
     textoTarefa.value = '';
+  }
+}
+
+function eventBtnAdiciona() { // Evento para o botão que adiciona itens.
+  btnAdiciona.addEventListener('click', function () {
+    craiItem();
   });
 }
 
@@ -51,8 +86,16 @@ function eventBtnLimpaConcluidas() {
   });
 }
 
+function eventBtnSalvarTarefas() {
+  btnSalvaTarefas.addEventListener('click', function () {
+    salvaListaNoStorage();
+  });
+}
+
 window.onload = function () {
   eventBtnAdiciona();
   eventBtnLimpa();
   eventBtnLimpaConcluidas();
+  eventBtnSalvarTarefas();
+  carregaListaSalva();
 };
