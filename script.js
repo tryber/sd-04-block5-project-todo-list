fb5f02ed0dac722008f83dda7d491500c8b45066
deboracosilveira <deboracosilveira@gmail.com>
2020-04-11 11:01:19 -0300

    requirement-7

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let newTask = document.getElementById("texto-tarefa");
 let button = document.getElementById("criar-tarefa");
 let list = document.getElementById("lista-tarefas");
+
 // New item is created when the user press the button
 button.addEventListener("click", addItem);
 function addItem() {  
@@ -10,3 +11,8 @@ function addItem() {
   list.appendChild(task)
   newTask.value = "";
 }
+// Change buttons cursor type
+let buttons = document.querySelectorAll("button")
+for( i = 0; i < buttons.length; i+=1) {
+  buttons[i].style.cursor = "pointer"
+};
