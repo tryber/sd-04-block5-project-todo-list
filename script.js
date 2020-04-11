529e9aa732b2195a270d30a670d83fec44a8e1ea
deboracosilveira <deboracosilveira@gmail.com>
2020-04-11 15:33:03 -0300

    requirement-8

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 // Variables
 let newTask = document.getElementById("texto-tarefa");
 let button = document.getElementById("criar-tarefa");
-let list = document.getElementById("lista-tarefas");
+// let list = document.querySelector("lista-tarefas");
+
 
 // New item is created when the user press the button
 button.addEventListener("click", addItem);
-function addItem() {  
-  let task = document.createElement("li");
-  task.innerText = newTask.value;
-  list.appendChild(task)
-  newTask.value = "";
+function addItem() {
+  if(newTask.value !== "") { //Do not accept an empty taks
+    let task = document.createElement("li");
+    let list = document.getElementById("lista-tarefas")
+    task.innerText = newTask.value;
+    list.appendChild(task)
+    newTask.value = "";
+  }
+  selectTask();
+ 
+}
+// Select task and color background
+function selectTask() {
+  let task = document.getElementsByTagName("li");
+  for( i = 0; i < task.length; i+=1) {
+    task[i].addEventListener("click", function(event) {
+    event.target.classList.add("selected");
+    event.target.style.backgroundColor = "rgb(128,128,128)"
+    })
+  }
 }
 // Change buttons cursor type
-let buttons = document.querySelectorAll("button")
+let buttons = document.querySelectorAll("button");
 for( i = 0; i < buttons.length; i+=1) {
-  buttons[i].style.cursor = "pointer"
+  buttons[i].style.cursor = "pointer";
 };
+
