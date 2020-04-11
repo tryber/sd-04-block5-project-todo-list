e990151a20f413a56749fdeb96cb5444c0a1053d
deboracosilveira <deboracosilveira@gmail.com>
2020-04-11 18:54:52 -0300

    requirement-9

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ function addItem() {
     task.style.cursor = "pointer";//change cursor type
     newTask.value = "";
   }
-  selectTask();
+  // selectTask();//one click
+  completedTask()//double click
 }
 // Select task and change color background
 function selectTask() {
@@ -31,4 +32,34 @@ let buttons = document.querySelectorAll("button");
 for( i = 0; i < buttons.length; i+=1) {
   buttons[i].style.cursor = "pointer";
 };
+// Setting a completed task
+function completedTask() {
+  let task = document.getElementsByTagName("li");
+  
+  for(i = 0; i < task.length; i+=1) {
+    task[i].addEventListener("dblclick", function(event) {
+      event.target.classList.add("completed");
+      event.target.style.textDecoration = "line-through"
+      undoCompletedTask()
+    })
+  }
 
+}
+// Undo completed task    
+function undoCompletedTask() {
+  let completed = document.querySelectorAll(".completed");
+  for(i = 0; i < completed.length; i+=1) {
+    if(completed) {
+      completed[i].addEventListener("dblclick", function(event) {
+        event.target.classList.remove("completed");
+        event.target.style.textDecoration = "none"
+      })
+    }
+  }
+}
+
+
+
+       
+
+      
