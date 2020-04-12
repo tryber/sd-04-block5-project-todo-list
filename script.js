bb2ddea39c78639dd1741952c29ca0d9f673f482
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-04-12 10:32:41 -0300

    step11

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
+let botaoCriar = document.getElementById('criar-tarefa');
+let lista = document.getElementById('lista-tarefas');
+
+function criarTarefa() {
+  let inputTarefa = document.getElementById('texto-tarefa').value;
+  let item = document.createElement('li');
+  item.innerHTML = inputTarefa;
+  lista.appendChild(item);
+  document.getElementById('texto-tarefa').value = '';
+}
+function limparTarefas() {
+  document.getElementById("lista-tarefas").innerHTML = '';
+}
+function limparTarefasConcluidas() {
+  let tarefasCompletadas = document.querySelectorAll(".completed");
+  for (let i = 0; i < tarefasCompletadas.length; i += 1) {
+    document.getElementById("lista-tarefas").removeChild(tarefasCompletadas[i]);
+  }
+}
+
 window.onload = function () {
 
-  let botaoCriar = document.getElementById('criar-tarefa');
-  let lista = document.getElementById('lista-tarefas');
-
   botaoCriar.addEventListener('click', criarTarefa);
-
-
-  function criarTarefa() {
-    let inputTarefa = document.getElementById('texto-tarefa').value;
-    let item = document.createElement('li');
-    item.innerHTML = inputTarefa;
-    lista.appendChild(item);
-    document.getElementById('texto-tarefa').value = '';
-  }
-
-  // document.addEventListener("mouseover", function (event) {
-  //   if (event.target && event.target.nodeName == "BUTTON") {
-  //     event.target.style.cursor = 'pointer';
-  //   }
-  // });
-
-  // document.getElementById("lista-tarefas").addEventListener("mouseover", function (event) {
-  //   if (event.target && event.target.nodeName == "LI") {
-  //     event.target.style.cursor = 'pointer';
-  //   }
-  // });
+  document.getElementById('apaga-tudo').addEventListener('click', limparTarefas);
+  document.getElementById('remover-finalizados').addEventListener('click', limparTarefasConcluidas);
 
   document.getElementById("lista-tarefas").addEventListener("click", function (event) {
     if (event.target && event.target.nodeName == "LI") {
@@ -40,22 +38,18 @@ window.onload = function () {
     }
   });
 
-  document.getElementById('apaga-tudo').addEventListener('click', limparTarefas);
-
-  function limparTarefas() {
-    document.getElementById("lista-tarefas").innerHTML = '';
-  }
-
-  document.getElementById('remover-finalizados').addEventListener('click', limparTarefasConcluidas);
-
-  function limparTarefasConcluidas() {
-    let tarefasCompletadas = document.getElementsByClassName("completed");
-    for (let i = 0; i < tarefasCompletadas.length; i+=1){
-      document.getElementById("lista-tarefas").removeChild(tarefasCompletadas[i]);      
-    }
-  }
-
-
 }
+  // --> other ways to add cursor events <--
+  // document.addEventListener("mouseover", function (event) {
+  //   if (event.target && event.target.nodeName == "BUTTON") {
+  //     event.target.style.cursor = 'pointer';
+  //   }
+  // });
+
+  // document.getElementById("lista-tarefas").addEventListener("mouseover", function (event) {
+  //   if (event.target && event.target.nodeName == "LI") {
+  //     event.target.style.cursor = 'pointer';
+  //   }
+  // });
 
 
