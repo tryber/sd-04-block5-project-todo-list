36f4ceb80343e48e12eaf39b30393fd3dbe3477e
danilossousa <danilosantossousa@gmail.com>
2020-04-12 15:34:43 -0300

    v.0.5.0.1

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-window.onload = function () {
+const butAddTask = document.getElementById('criar-tarefa');
+const olTasks = document.getElementById('lista-tarefas');
+const inptasks = document.getElementById('texto-tarefa');
 
+function behaviors() {
+  butAddTask.addEventListener('click', function () {
+    const txtTask = document.getElementById('texto-tarefa').value;
+
+    if (txtTask !== '') {
+      const li = document.createElement('li');
+
+      olTasks.appendChild(li).innerText = txtTask;
+
+      inptasks.value = '';
+    }
+  });
+}
+
+window.onload = function () {
+  behaviors();
 };
