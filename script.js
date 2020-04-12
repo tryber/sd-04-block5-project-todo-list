6ea6d8fe5806617b461e071fd69337f69ed77f61
danilossousa <danilosantossousa@gmail.com>
2020-04-12 19:14:07 -0300

    v.0.11.0.3

--- script.js
+++ script.js
@@ -5,44 +5,38 @@ const inptasks = document.getElementById('texto-tarefa');
 const butDelTasks = document.getElementById('apaga-tudo');
 const butDelFinish = document.getElementById('remover-finalizados');
 
-function behaviors() {
-  butAddTask.addEventListener('click', function () {
-    const txtTask = document.getElementById('texto-tarefa').value;
+butAddTask.addEventListener('click', function () {
+  const txtTask = document.getElementById('texto-tarefa').value;
 
-    if (txtTask !== '') {
-      const li = document.createElement('li');
+  if (txtTask !== '') {
+    const li = document.createElement('li');
 
-      li.style.cursor = 'pointer';
-      li.addEventListener('mouseover', function (e) { e.target.style.backgroundColor = 'rgb(128 , 128 , 128)'; });
-      li.addEventListener('mouseout', function (e) { e.target.style.backgroundColor = 'rgb(103 , 230 , 141)'; });
-      li.addEventListener('dblclick', function (e) {
-        if (e.target.className === '') {
-          e.target.className = 'completed';
-        } else {
-          e.target.className = '';
-        }
-      });
-
-      olTasks.appendChild(li).innerText = txtTask;
-      inptasks.value = '';
-    }
-  });
-
-  butDelTasks.addEventListener('click', function () {
-    while (olTasks.firstChild) {
-      olTasks.removeChild(olTasks.firstChild);
-    }
-  });
-
-  butDelFinish.addEventListener('click', function () {
-    for (i = 0; i < olTasks.children.length; i += 1) {
-      if (olTasks.children[i].className === 'completed') {
-        olTasks.removeChild(olTasks.children[i]);
+    li.style.cursor = 'pointer';
+    li.addEventListener('mouseover', function (e) { e.target.style.backgroundColor = 'rgb(128 , 128 , 128)'; });
+    li.addEventListener('mouseout', function (e) { e.target.style.backgroundColor = 'rgb(103 , 230 , 141)'; });
+    li.addEventListener('dblclick', function (e) {
+      if (e.target.className === '') {
+        e.target.className = 'completed';
+      } else {
+        e.target.className = '';
       }
-    }
-  });
-}
+    });
 
-window.onload = function () {
-  behaviors();
-};
+    olTasks.appendChild(li).innerText = txtTask;
+    inptasks.value = '';
+  }
+});
+
+butDelTasks.addEventListener('click', function () {
+  while (olTasks.firstChild) {
+    olTasks.removeChild(olTasks.firstChild);
+  }
+});
+
+butDelFinish.addEventListener('click', function () {
+  for (i = 0; i < olTasks.children.length; i += 1) {
+    if (olTasks.children[i].className === 'completed') {
+      olTasks.removeChild(olTasks.children[i]);
+    }
+  }
+});
