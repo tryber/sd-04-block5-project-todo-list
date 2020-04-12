479b9d9965f9ae2df494b61a4cf67f082bee8738
wistyk <leonardoxw@gmail.com>
2020-04-12 20:44:09 -0300

    1-9 CC ok

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const adicionar = document.querySelector('#criar-tarefa');
 adicionar.addEventListener('click', function () {
   const item = document.querySelector('#texto-tarefa');
   const lista = document.createElement('li');
+  const apagarT = document.querySelector('#apaga-tudo');
   lista.innerText = item.value;
   item.value = '';
 
@@ -14,5 +15,13 @@ adicionar.addEventListener('click', function () {
 
   lista.addEventListener('mouseleave', function () {
     lista.style.backgroundColor = 'white';
+  });
+
+  lista.addEventListener('dblclick', function () {
+    lista.classList.toggle('completed');
+  });
+
+  apagarT.addEventListener('click', function () {
+    lista.remove();
   });
 });
