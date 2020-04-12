cb54357312ba1980bba9ca1310e57bf963971078
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-04-12 12:38:07 -0300

    modificadas funçoes loadlist e criarTarefa para salvar tarefas completas

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const buttonCriarTarefa = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
-function criarTarefa(tarefa) {
+function criarTarefa(tarefa, status) {
   const li = document.createElement('li');
   li.innerHTML = tarefa;
+  if (status != undefined) li.classList.add(status);
   ol.appendChild(li);
 }
 
@@ -46,7 +47,7 @@ removerFinalizados.addEventListener('click', function () {
 
 function saveList(list) {
   for (let i = 0; i < list.length; i += 1) {
-    localStorage.setItem(i, list[i].innerHTML);
+    localStorage.setItem(i, list[i].innerHTML + '|' + list[i].classList[1]);
   }
 }
 
@@ -59,7 +60,8 @@ function loadList() {
   let index = 0;
   while (localStorage.getItem(index) !== null) {
     const savedValue = localStorage.getItem(index);
-    criarTarefa(savedValue);
+    const tarefaStatus = savedValue.split('|');
+    criarTarefa(tarefaStatus[0], tarefaStatus[1]);
     index += 1;
   }
 }
