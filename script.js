2996b410788043a55ca9c243cc89cdeade0ce546
Julio Cesar <stininmatic@hotmail.com>
2020-04-13 17:07:52 -0300

    Constantes

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
 window.onload = function(){
 
 //CAIXA DE TEXTO
-    let inputTask = document.querySelector("#texto-tarefa");
+    const inputTask = document.querySelector("#texto-tarefa");
 //BOTÃO CRIAR
-    let buttonCreate = document.querySelector("#criar-tafefa");
+    const buttonCreate = document.querySelector("#criar-tafefa");
 //VARIÁVEL DA LISTA
-    let toDoList = document.querySelector('#lista-tarefas');
-
+    const toDoList = document.querySelector('#lista-tarefas');
 
     buttonCreate.addEventListener('click', insertItem);
 
 //FUNÇÃO DESTACA ITEM
     function pintaFundo(event){
-        let selecionado = event.target;
+        const selecionado = event.target;
         selecionado.classList.add("selected");
     }
 
 //FUNÇÃO PARA RISCAR
     function riscada(event){
-        let feito = event.target.style.textDecoration;
-        if(feito != 'line-through'){
-            feito.classList.add("completed");
-        }else{
-        feito.classList.remove("completed")
-
-
+        const completo = event.target;
+        completo.classList.add('completed');
     }
-}
-
-function desmarcar(){
-    let itemMarcado = document.querySelector(".completed");
-    itemMarcado.classList.remove("completed");
-}
-
 
 //FUNÇÃO DE INSERIR ITEM
     function insertItem(){
