a59c165b37eb600455536621a86d1c38ec205620
Leandro Marten <leandromarten1@gmail.com>
2020-04-13 16:23:48 -0300

    Nao deixa acrescentar tarefa vazia

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 let tarefa = document.querySelector('#texto-tarefa');
 let lista = document.querySelector('#lista-tarefas');
 let btnAdd = document.querySelector('#criar-tarefa');
+let btnDel = document.querySelector('#apaga-tudo');
 
 // Adiciona uma tarefa a lista
 function addTarefa(){
   let toDo = document.createElement('li');
-  toDo.innerHTML = tarefa.value;
-  lista.appendChild(toDo);
-  tarefa.value = null;
+  if(tarefa.value != ""){
+    toDo.innerHTML = tarefa.value;
+    lista.appendChild(toDo);
+    tarefa.value = null;
+  }else{
+    alert('Você precisa digitar uma tarefa antes!');
+  }
 };
 
 lista.addEventListener('click', function(event){
@@ -25,7 +30,16 @@ function tarefaOk(){
     unicaTarefa.classList.add('completed');
   }
 }
+// Apagar lista
+function deleteTarefas(){
+  let todasTarefas = document.querySelectorAll('li');
+  for (let i = 0; i < todasTarefas.length; i += 1){
+    lista.removeChild(todasTarefas[i]);
+  }
+}
 
 // Chamando o Evento aos botões
 btnAdd.addEventListener('click', addTarefa);
 lista.addEventListener('dblclick', tarefaOk);
+btnDel.addEventListener('click', deleteTarefas);
+
