27b092b66e8e8f3036d79125f35cb20d17e8b7fb
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-04-13 18:41:29 -0300

    fix/fixed input clear

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Query selectors
 const addTaskBtn = document.querySelector('.criar-tarefa');
 const taskList = document.querySelector('.lista-tarefas');
+const text = document.querySelector('.texto-tarefa');
 
 // Create task function
 function createTask() {
   const task = document.createElement('li');
-  const text = document.querySelector('.texto-tarefa');
   task.innerHTML = text.value;
   return task;
 }
@@ -13,4 +13,5 @@ function createTask() {
 // Add task event listener
 addTaskBtn.addEventListener('click', () => {
   taskList.appendChild(createTask());
+  text.value = '';
 });
