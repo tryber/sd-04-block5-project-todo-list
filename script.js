fa9296d6912df7d0ac586d2288d5182e291e8526
Lucas Barbosa Rosa <lucaslucas2008@gmail.com>
2020-04-13 17:28:32 -0300

    Add requisition 3-6

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+const lista = document.getElementById("lista-tarefas");
+const buttonAdd = document.getElementById("criar-tarefa");
+const inputText = document.getElementById("texto-tarefa");
 
+function AddElemento()
+{
+    const elemento = document.createElement('li');
+    const informacao = inputText.value;
+    elemento.innerHTML = informacao;
+    //elemento.setAttribute('class', 'textos');
+    lista.appendChild(elemento);
+    inputText.value = "";
+}
 
-window.onload = function 
+function Iniciar()
+{
+    buttonAdd.addEventListener("click", function() 
+    {
+        AddElemento();
+    });
+}
+
+window.onload = function () 
 {
     Iniciar();
 };
