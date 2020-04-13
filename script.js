c9d69a01678080d882d02c179b10ed778524d614
deboracosilveira <deboracosilveira@gmail.com>
2020-04-13 19:29:34 -0300

    bonus-1 ajust

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let deleteAll = document.getElementById("apaga-tudo");
 let deleteCompletedTasks = document.getElementById("remover-finalizados");
 let list = document.getElementById("lista-tarefas")
 let saveTasks =document.getElementById("salvar-tarefas")
-let task = document.getElementsByTagName("li")
 
 // New item is created when the user press the button
 createTask.addEventListener("click", addItem);
@@ -15,18 +14,15 @@ function addItem() {
     let list = document.getElementById("lista-tarefas")
     task.innerText = newTask.value;
     list.appendChild(task)
+    task.style.cursor = "pointer";//change cursor type
     newTask.value = "";
   }
-  
-  
-}
-list.addEventListener("click", function() {
   selectTask();//one click
   completedTask()//double click
-  undoCompletedTask()//double click in completed tasks
-})
+}
 // Select task and change color background
 function selectTask() {
+  let task = document.getElementsByTagName("li");
   for( i = 0; i < task.length; i+=1) {
     task[i].addEventListener("click", function(event) {
       event.target.classList.add("selected");
@@ -47,6 +43,7 @@ function completedTask() {
     task[i].addEventListener("dblclick", function(event) {
       event.target.classList.add("completed");
       event.target.style.textDecoration = "line-through"
+      undoCompletedTask()
     })
   }
 }
@@ -69,12 +66,16 @@ function deleteCompleted() {
   for(i = 0; i < items.length; i+=1) {
     items[i].parentNode.removeChild(items[i]); 
   }
+  selectTask();//one click
+  completedTask()//double click
 }
 // Delete all tasks
 deleteAll.addEventListener("click", deleteAllTasks)
 function deleteAllTasks() {
   let items = document.querySelector("ol")
   items.innerHTML = "";
+  selectTask();//one click
+  completedTask()//double click
 }
 // Set Local storage
 saveTasks.addEventListener("click", setLocalStorage);
@@ -96,7 +97,8 @@ window.onload = function() {
   let index = localStorage.getItem("numberOfItems");
   for( let i = 0; i < index; i+=1) {
     let item = document.createElement("li")
-    item.className = localStorage.getItem(`Class ${i}`)//loas class content
+    item.className = localStorage.getItem(`Class ${i}`)
     list.appendChild(item).innerHTML = localStorage.getItem(`Task ${i}`)//load list content
   }  
+  addItem()
 }
