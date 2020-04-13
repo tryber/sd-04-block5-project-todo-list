96b5b252fa4481cb731d62077f5dd32e2cfce0da
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-04-13 16:24:43 -0300

    Tentativa de passar pelos requisitos 5 e 10

---
+++ script.js
@@ -0,0 +1,22 @@
+const botaoAdiciona = document.getElementById('criar-tarefa');
+
+function acrescentaItem() {
+  const lista = document.createElement('li');
+  const textoTarefa = document.getElementById('texto-tarefa').value;
+  lista.innerHTML = textoTarefa;
+  document.getElementById('lista-tarefas').appendChild(lista);
+  document.getElementById('texto-tarefa').value = '';
+}
+
+botaoAdiciona.addEventListener('click', acrescentaItem);
+
+const botaoApaga = document.getElementById('apaga-tudo');
+
+function apagaItens() {
+  const li = document.querySelectorAll('li');
+  for (let i = 0; i < li.length; i = 1 + i) {
+    document.getElementById('lista-tarefas').removeChild(li[i]);
+  }
+}
+
+botaoApaga.addEventListener('click', apagaItens);
