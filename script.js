c3887b3b71d13960275c1250cae48df374519284
Leandro Marten <leandromarten1@gmail.com>
2020-04-13 15:06:32 -0300

    Inicio JS-variaveis

---
+++ script.js
@@ -0,0 +1,4 @@
+// Declarando os campos necessários
+let tarefa = document.querySelector('#texto-tarefa');
+let lista = document.querySelector('#lista-tarefas');
+let btnAdd = document.querySelector('#criar-tarefa');
