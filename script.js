902d147178378c002a1933d02f6482eccc7adb27
Tiago Emanuel <trsilva284@gmail.com>
2020-04-13 19:19:30 -0300

    iniciando projeto

---
+++ script.js
@@ -0,0 +1,33 @@
+//botão adcicionar tarefa
+let criartarefa = document.querySelector("#criar-tarefa");
+console.log(criartarefa);
+
+let listaDeTarefas = document.querySelector(".lista-tarefas");
+
+criartarefa.addEventListener("click", function(event){
+    let form = document.querySelector("#adiciona");  
+    if(form.textoTarefa.value == "")
+    {
+        return;
+    } 
+    let tarefaLista = form.textoTarefa.value;
+    montaLista(tarefaLista);
+    tarefaLista ="";
+});
+
+listaDeTarefas.addEventListener("dblclick", function(e){
+    e.target.classList.toggle("completed");
+});
+
+listaDeTarefas.addEventListener("click", function(e){     
+   e.target.classList.toggle("alteracor");
+});
+
+//Adiciona a tarefa na lista com a classe (lista-tarefas)
+function montaLista(tarefaLista){
+    let Tarefas = document.querySelector("#lista-tarefas");
+    let lista = document.createElement("li");
+    lista.textContent = tarefaLista;
+    lista.classList.add("lista-tarefas");
+    Tarefas.appendChild(lista);
+}
