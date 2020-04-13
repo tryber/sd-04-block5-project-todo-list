a61bf5f97e76ec6d8cec51fc3c5df5bca1bae5d1
Leandro Marten <leandromarten1@gmail.com>
2020-04-13 15:13:55 -0300

    function addTarefa

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 let tarefa = document.querySelector('#texto-tarefa');
 let lista = document.querySelector('#lista-tarefas');
 let btnAdd = document.querySelector('#criar-tarefa');
+
+function addTarefa(){
+  let toDo = document.createElement('li');
+  toDo.innerHTML = tarefa.value;
+  lista.appendChild(toDo);
+}
