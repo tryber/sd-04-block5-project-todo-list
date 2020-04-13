9d7ab036da473bd178f8c0a1894ec8c59c37e312
danilossousa <danilosantossousa@gmail.com>
2020-04-13 02:09:18 -0300

    v.0.12.0.6

--- script.js
+++ script.js
@@ -5,30 +5,33 @@ const inptasks = document.getElementById('texto-tarefa');
 const butDelTasks = document.getElementById('apaga-tudo');
 const butDelFinish = document.getElementById('remover-finalizados');
 const butSaveTasks = document.getElementById('salvar-tarefas');
-// const storageClassValues = [];
 
-checkStorage();
+function addEvents(elm) {
+  elm.style.cursor = 'pointer';
+  elm.addEventListener('mouseover', function (e) { e.target.style.backgroundColor = 'rgb(128 , 128 , 128)'; });
+  elm.addEventListener('mouseout', function (e) { e.target.style.backgroundColor = 'rgb(103 , 230 , 141)'; });
+  elm.addEventListener('dblclick', function (e) {
+    if (e.target.className) {
+      e.target.className = '';
+    } else {
+      e.target.className = 'completed';
+    }
+  });
+}
 
 function checkStorage() {
   if (localStorage.length > 0) {
     for (i = 0; i < localStorage.length; i += 1) {
       const li = document.createElement('li');
 
-      li.style.cursor = 'pointer';
-      li.addEventListener('mouseover', function (e) { e.target.style.backgroundColor = 'rgb(128 , 128 , 128)'; });
-      li.addEventListener('mouseout', function (e) { e.target.style.backgroundColor = 'rgb(103 , 230 , 141)'; });
-      li.addEventListener('dblclick', function (e) {
-        if (e.target.className) {
-          e.target.className = '';
-        } else {
-          e.target.className = 'completed';
-        }
-      });
+      addEvents(li);
 
-      olTasks.appendChild(li).innerText = localStorage.getItem(`key${i}`);
+      olTasks.appendChild(li).innerText = localStorage.getItem(`task${i}`);
     }
   }
 }
+
+checkStorage();
 
 butAddTask.addEventListener('click', function () {
   const txtTask = document.getElementById('texto-tarefa').value;
@@ -36,16 +39,7 @@ butAddTask.addEventListener('click', function () {
   if (txtTask !== '') {
     const li = document.createElement('li');
 
-    li.style.cursor = 'pointer';
-    li.addEventListener('mouseover', function (e) { e.target.style.backgroundColor = 'rgb(128 , 128 , 128)'; });
-    li.addEventListener('mouseout', function (e) { e.target.style.backgroundColor = 'rgb(103 , 230 , 141)'; });
-    li.addEventListener('dblclick', function (e) {
-      if (e.target.className) {
-        e.target.className = '';
-      } else {
-        e.target.className = 'completed';
-      }
-    });
+    addEvents(li);
 
     olTasks.appendChild(li).innerText = txtTask;
     inptasks.value = '';
@@ -60,7 +54,7 @@ butDelTasks.addEventListener('click', function () {
   localStorage.clear();
 });
 
-butDelFinish.addEventListener('click', function () {  
+butDelFinish.addEventListener('click', function () {
   for (i = 0; i < olTasks.children.length; i += 1) {
     if (olTasks.children[i].className === 'completed') {
       olTasks.removeChild(olTasks.children[i]);
@@ -69,13 +63,11 @@ butDelFinish.addEventListener('click', function () {
   }
 });
 
-butSaveTasks.addEventListener('click', function () {  
+butSaveTasks.addEventListener('click', function () {
   for (i = 0; i < olTasks.children.length; i += 1) {
     const value = olTasks.children[i].innerText;
-    const key = `key${i}`;
+    const key = `task${i}`;
 
     localStorage.setItem(key, value);
-
-    // storageClassValues[i] = olTasks.children[i].className;
   }
 });
