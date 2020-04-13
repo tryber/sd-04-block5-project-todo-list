2a7302096f52ee4b7595dc3890e052285c851399
AliceAAtalla <aliceatalla36@gmail.com>
2020-04-13 15:26:59 -0300

    Save button and update change class

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ let addTarefa = document.querySelector('#criar-tarefa');
 let clearAll = document.querySelector('#apaga-tudo');
 let inpText = document.querySelector('#texto-tarefa');
 let ol = document.querySelector('#lista-tarefas');
-let item = document.getElementsByClassName('task');
 let btnFinalizados = document.querySelector('#remover-finalizados');
 let btnSelecionados = document.querySelector('#remover-selecionado');
 let btnSalvar = document.querySelector('#salvar-tarefas');
 let btnMoverCima = document.querySelector('#mover-cima');
 let btnMoverBaixo = document.querySelector('#mover-baixo');
+
+if (typeof (Storage) !== "undefined") {
+  ol.innerHTML = localStorage.lista;
+}
+
+btnSalvar.addEventListener('click', function () {
+  if (ol.innerHTML == '') {
+    alert('Não existe tarefas para salvar');
+  } else {
+    localStorage.setItem('lista', ol.innerHTML);
+  }
+});
+
 
 //Adicionar tarefas na lista
 addTarefa.addEventListener('click', function () {
@@ -15,25 +27,27 @@ addTarefa.addEventListener('click', function () {
     let li = document.createElement('li');
     let valor = document.getElementById('texto-tarefa').value;
     let text = document.createTextNode(valor);
-    li.addEventListener('click', function () {
-      trocaClass(li, 'selected');
-    });
-    li.addEventListener('dblclick', function () {
-      trocaClass(li, 'completed');
-    });
     li.appendChild(text); //adicionar valor a li
     ol.appendChild(li); //adicionar li em ol
     li.className = 'task';
     inpText.focus(); //deixar o cursor na caixa de texto apos adicionar
     inpText.value = ''; //limpar a caixa de texto
+
   }
 });
 
+ol.addEventListener("click", function (e) {
+  trocaClass(e, 'selected');
+})
+ol.addEventListener("dblclick", function (e) {
+  trocaClass(e, 'completed');
+})
+
 function trocaClass(elemento, classe) {
-  if (elemento.classList.contains(classe) == true) {
-    elemento.classList.remove(classe);
+  if (elemento.target.classList.contains(classe) == true) {
+    elemento.target.classList.remove(classe);
   } else {
-    elemento.classList.add(classe);
+    elemento.target.classList.add(classe);
   }
 }
 
@@ -45,19 +59,19 @@ clearAll.addEventListener('click', function () {
 
 btnFinalizados.addEventListener('click', function () {
   clearFimSele('completed');
-  
+
 })
 
 
-btnSelecionados.addEventListener('click', function(){
+btnSelecionados.addEventListener('click', function () {
   clearFimSele('selected');
-    
-  })
+
+})
 // COLOCANDO TUDO NUMA FUNÇAO:
-function  clearFimSele(classe){
+function clearFimSele(classe) {
   let li = document.querySelectorAll('li');
-  for(let i=0; i<li.length;i+=1){
-    if(li[i].classList.contains(classe)==true){
+  for (let i = 0; i < li.length; i += 1) {
+    if (li[i].classList.contains(classe) == true) {
       li[i].remove();
     }
   }
