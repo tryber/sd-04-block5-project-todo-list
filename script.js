5cf6b2780ec8842beab9647c99e13105f226dfbb
frederico-campello <fredfmc33@gmail.com>
2020-04-13 19:03:49 -0300

    Iniciando o projeto

--- script.js
+++ script.js
@@ -4,17 +4,42 @@ window.onload = function() {
   const listaDeTarefas = document.getElementById('lista-tarefas');
   const botaoRemoverFinalizados = document.getElementById('remover-finalizados');
   const botaoLimparLista = document.getElementById('apaga-tudo');
-
+  //
   
-  // Requisito 5 
   function criarTarefa (event) {
-  var item = document.createElement('li');
-  var tarefa = document.createTextNode(tarefaInserida.value);
+  const item = document.createElement('li');
+  const tarefa = document.createTextNode(tarefaInserida.value);
   item.appendChild(tarefa);
   listaDeTarefas.appendChild(item);
   tarefaInserida.value = "";
   }
   botaoAdicionar.addEventListener('click', criarTarefa);
-
+  //
   
+  function alteraLiBackground (event) {
+  console.log(event.target);
+  event.target.classList.add('item-cinza');
+  }
+  listaDeTarefas.addEventListener('click', alteraLiBackground);
+  //
+
+  function itemCompleto (event) {
+  event.target.classList.remove('item-cinza');
+  event.target.classList.add('completed');
+  }
+  listaDeTarefas.addEventListener('dblclick', itemCompleto);
+  //
+
+  function apagaFinalizadas (event) {
+  document.querySelector('.completed').innerHTML = "";
+  }
+  botaoRemoverFinalizados.addEventListener ('click', apagaFinalizadas);
+  //
+  
+  function apagaLista (event) {
+  listaDeTarefas.innerHTML = "";
+  }
+  botaoLimparLista.addEventListener('click', apagaLista);
+  //
+
 }
