6e8eafb6b8a8575cd0eb1ba61650a20c9937d581
igorgbr <igorgphotoarte@gmail.com>
2020-04-13 00:57:12 -0300

    primeira alteraçao na logicaa

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const btnTarefa = document.querySelector('#criar-tarefa');
 
+// btnTarefa.addEventListener('click', function(){
+//     let inputTarefa = document.querySelector('#texto-tarefa').value;
+//     var liTarefa = document.createElement("LI");         
+//     var textnode = document.createTextNode(inputTarefa);        
+//     liTarefa.appendChild(textnode);                              
+//     document.getElementById("lista-tarefas").appendChild(liTarefa); 
+//     inputTarefa = ' ';
+//     console.log(inputTarefa.value)
+// })
+
+
 btnTarefa.addEventListener('click', function(){
-    const inputTarefa = document.querySelector('#texto-tarefa').value;
-    var liTarefa = document.createElement("LI");         
-    var textnode = document.createTextNode(inputTarefa);        
-    liTarefa.appendChild(textnode);                              
-    document.getElementById("lista-tarefas").appendChild(liTarefa); 
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("texto-tarefa").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("Você deve escrever algo");
+    } else {
+      document.getElementById("lista-tarefas").appendChild(li);
+    }
+    document.getElementById("texto-tarefa").value = "";
 })
