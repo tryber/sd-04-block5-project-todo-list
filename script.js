2c85ecd0608111393105c4da162ddbe627acc897
Julio Cesar <stininmatic@hotmail.com>
2020-04-13 16:35:36 -0300

    alteração de html

--- script.js
+++ script.js
@@ -2,54 +2,31 @@ window.onload = function(){
 
 //CAIXA DE TEXTO
     let inputTask = document.querySelector("#texto-tarefa");
-
 //BOTÃO CRIAR
     let buttonCreate = document.querySelector("#criar-tafefa");
-
 //VARIÁVEL DA LISTA
     let toDoList = document.querySelector('#lista-tarefas');
 
+
     buttonCreate.addEventListener('click', insertItem);
-
-//FUNÇÃO DE DESTAQUE CINZA
-/*    function highlightItem(){
-        const fundoLista = this.document.querySelectorAll("#fundo-lista");
-
-        for (let i = 0; i < fundoLista.length; i++) {
-            fundoLista[i].addEventListener('click', function(event){
-                let selectedItem = event.target;
-                pintaFundo(selectedItem);
-            });
-        }
-    }
-*/
 
 //FUNÇÃO DESTACA ITEM
     function pintaFundo(event){
-        event.target.style.backgroundColor = 'grey';
+        let selecionado = event.target;
+        selecionado.classList.add("selected");
     }
-
- //   function pintaFundo(a){
- //       a.classList.add('selected');
- //       a.addEventListener("click", apagaFundo);
- //       let selecionado = document.querySelector(".selected");
- //       selecionado.classList.remove("selected");
- //   }
-
-
- //   highlightItem()
-    
-//    function apagaFundo(){
-//    document.querySelector(".selected").classList.remove("selected");
- //   }
-  
 
 //FUNÇÃO PARA RISCAR
     function riscada(event){
-        let feito = event.target;
-        feito.classList.add("completed")
+        let feito = event.target.style.textDecoration;
+        if(feito != 'line-through'){
+            feito.classList.add("completed");
+        }else{
+        feito.classList.remove("completed")
+
 
     }
+}
 
 function desmarcar(){
     let itemMarcado = document.querySelector(".completed");
@@ -68,11 +45,34 @@ function desmarcar(){
         listItem.addEventListener("click", pintaFundo);
         listItem.addEventListener('dblclick', riscada);
         
-        
     }
 
 }
 
+//FUNÇÃO DE DESTAQUE CINZA
+/*    function highlightItem(){
+        const fundoLista = this.document.querySelectorAll("#fundo-lista");
+
+        for (let i = 0; i < fundoLista.length; i++) {
+            fundoLista[i].addEventListener('click', function(event){
+                let selectedItem = event.target;
+                pintaFundo(selectedItem);
+            });
+        }
+    }
+*/
+
+ //   function pintaFundo(a){
+ //       a.classList.add('selected');
+ //       a.addEventListener("click", apagaFundo);
+ //       let selecionado = document.querySelector(".selected");
+ //       selecionado.classList.remove("selected");
+ //   }
 
 
-
+ //   highlightItem()
+    
+//    function apagaFundo(){
+//    document.querySelector(".selected").classList.remove("selected");
+ //   }
+  
