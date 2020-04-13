3ba139d7e015454e2c5ced0fb3b59c10e50055a3
Lucas Barbosa Rosa <lucaslucas2008@gmail.com>
2020-04-13 19:49:40 -0300

    Changes in  requisition 7

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const lista = document.getElementById("lista-tarefas");
 const buttonAdd = document.getElementById("criar-tarefa");
 const inputText = document.getElementById("texto-tarefa");
 
+function Seleciona(event)
+{
+    event.target.className = "selected";
+}
+
 function AddElemento()
 {
     const elemento = document.createElement('li');
     const informacao = inputText.value;
     elemento.innerHTML = informacao;
     //elemento.setAttribute('class', 'textos');
+    elemento.addEventListener('click', function (event) { Seleciona(event); });
     lista.appendChild(elemento);
-    inputText.value = "";
+    inputText.value = '';
 }
 
 function Iniciar()
