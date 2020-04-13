2a8ac3261f8e27b205451f4678209b855e5e4e5e
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-04-13 19:03:47 -0300

    Tentativa de passar pelo requisito 9

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ function adicionaCor(event) {
   event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
 
+function tarefaFinalizada(event) {
+  if (event.target.className === 'completed') {
+    event.target.className = 'none';
+  } else {
+    event.target.className = 'completed';
+  }
+}
+
 function acrescentaItem() {
   const lista = document.createElement('li');
   const textoTarefa = document.getElementById('texto-tarefa').value;
   lista.innerHTML = textoTarefa;
   lista.addEventListener('click', adicionaCor);
+  lista.addEventListener('dblclick', tarefaFinalizada);
   document.getElementById('lista-tarefas').appendChild(lista);
   document.getElementById('texto-tarefa').value = '';
 }
