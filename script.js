8244f74eba94e995209d8be62a9afe41b2e74717
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-04-13 17:40:06 -0300

    feature/add task remake

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
 // Query selectors
 const addTaskBtn = document.querySelector('.criar-tarefa');
-const list = document.querySelector('.lista-tarefas');
+const taskList = document.querySelector('.lista-tarefas');
+
+// Create task function
+function createTask() {
+  const task = document.createElement('li');
+  const text = document.querySelector('.texto-tarefa');
+  task.innerHTML = text.value;
+  return task;
+}
 
 // Add task event listener
 addTaskBtn.addEventListener('click', () => {
-  const task = document.querySelector('.texto-tarefa').value;
-  const newTaskLi = document.createElement('li');
-  newTaskLi.classList.add('task-item');
-  const newTaskText = document.createElement('p');
-  newTaskText.innerHTML = task;
-  newTaskLi.appendChild(newTaskText);
-  list.appendChild(newTaskLi);
-
-  newTaskLi.addEventListener('click', () => {
-    const taskItem = document.querySelectorAll('.task-item');
-    taskItem.forEach((item) => {
-      item.style.backgroundColor = 'initial';
-    });
-    newTaskLi.style.backgroundColor = 'rgb(128 , 128 , 128)';
-  });
-
-  newTaskLi.addEventListener('dblclick', () => {
-    newTaskLi.classList.add('completed');
-  });
+  taskList.appendChild(createTask());
 });
