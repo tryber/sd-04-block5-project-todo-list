85e1689aa639b8c32769a5dd13935a19b65c32ac
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-04-14 16:09:06 -0300

    feature/added local storage

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 const addTaskBtn = document.querySelector('.criar-tarefa');
 const deleteAllBtn = document.querySelector('.apaga-tudo');
 const deleteCompletedBtn = document.querySelector('.remover-finalizados');
+const saveTasks = document.querySelector('.salvar-tarefas');
 const taskList = document.querySelector('.lista-tarefas');
 const text = document.querySelector('.texto-tarefa');
 
 // Create task element
-function createTask() {
+function createTask(liText) {
   const task = document.createElement('li');
   task.classList.add('task-li');
-  task.innerHTML = text.value;
+  task.innerHTML = liText;
   task.addEventListener('click', () => {
     const tasks = document.querySelectorAll('.task-li');
     tasks.forEach((item) => { if (item !== event.target) item.classList.remove('selected'); });
@@ -23,7 +24,7 @@ function createTask() {
 
 // Create task event listener
 addTaskBtn.addEventListener('click', () => {
-  if (text.value !== '') taskList.appendChild(createTask());
+  if (text.value !== '') taskList.appendChild(createTask(text.value));
   text.value = '';
 });
 
@@ -39,3 +40,24 @@ deleteCompletedBtn.addEventListener('click', () => {
     item.parentNode.removeChild(item);
   });
 });
+
+// Save tasks
+saveTasks.addEventListener('click', () => {
+  const tasks = document.querySelectorAll('.task-li');
+  const tasksArray = [];
+  tasks.forEach((item) => {
+    tasksArray.push(item.innerHTML);
+  });
+  localStorage.setItem('taskList', JSON.stringify(tasksArray));
+});
+
+// Load tasks
+function loadTasks() {
+  const savedTasks = localStorage.getItem('taskList');
+  const savedTasksArray = JSON.parse(savedTasks);
+  savedTasksArray.forEach((item) => {
+    taskList.appendChild(createTask(item));
+  });
+}
+
+loadTasks();
