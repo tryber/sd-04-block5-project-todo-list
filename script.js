7a82868c838b7812d5f8f6a3314183b87a1e39a3
Josias Viveiro <josiasviveiro@gmail.com>
2020-04-14 16:41:24 -0300

    js - codeclimate issues

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 window.onload = function () {
-    let textTask = document.getElementById("texto-tarefa");
-    let btnCreateTask = document.getElementById("criar-tarefa");
-    let taskList = document.getElementById("lista-tarefas");
+  let textTask = document.getElementById('texto-tarefa');
+  let btnCreateTask = document.getElementById('criar-tarefa');
+  let taskList = document.getElementById('lista-tarefas');
 
-    function createTask() {
-        let newTask = document.createElement("li");
-        //newTask.innerText = textTask.value;
-        newTask.innerHTML = textTask.value;
-        taskList.appendChild(newTask);
-        textTask.value = "";
-    }
+  function createTask() {
+    let newTask = document.createElement('li');
+    newTask.innerText = textTask.value;
+    taskList.appendChild(newTask);
+    textTask.value = '';
+  }
 
-    btnCreateTask.addEventListener("click", createTask);
-
-    console.log(taskList);
-    console.log(textTask);
-    console.log(btnCreateTask);
+  btnCreateTask.addEventListener('click', createTask);
 };
