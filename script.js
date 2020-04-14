28a0421bacfb016300bb5bac86f562424ad127a0
Josias Viveiro <josiasviveiro@gmail.com>
2020-04-14 16:08:57 -0300

    html, js - requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 window.onload = function () {
+    let textTask = document.getElementById("texto-tarefa");
+    let btnCreateTask = document.getElementById("criar-tarefa");
+    let taskList = document.getElementById("lista-tarefas");
 
+    function createTask() {
+        let newTask = document.createElement("li");
+        //newTask.innerText = textTask.value;
+        newTask.innerHTML = textTask.value;
+        taskList.appendChild(newTask);
+        textTask.value = "";
+    }
+
+    btnCreateTask.addEventListener("click", createTask);
+
+    console.log(taskList);
+    console.log(textTask);
+    console.log(btnCreateTask);
 };
