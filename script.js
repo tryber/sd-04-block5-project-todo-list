10d7a204309814b863d3092507ea1d35286a35a5
juniomelos <juniomelos@gmail.com>
2020-04-14 18:51:32 -0300

    codeclimate - 1

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const buttonDeleteList = document.getElementById('apaga-tudo');
 const removeFinished = document.getElementById('remover-finalizados');
 
 buttonCreateTask.addEventListener('click', function () {
-  let valueTaskText = taskText.value;
-  let li = document.createElement('li');
+  const valueTaskText = taskText.value;
+  const li = document.createElement('li');
   li.classList.add('listTask');
   listTask.appendChild(li).innerHTML = valueTaskText;
   taskText.value = '';
@@ -14,12 +14,12 @@ buttonCreateTask.addEventListener('click', function () {
 
 buttonDeleteList.addEventListener('click', function () {
   const listItems = document.querySelectorAll('.listTask');
-  for ( let i = 0; i < listItems.length; i += 1) {
+  for (let i = 0; i < listItems.length; i += 1) {
     listTask.removeChild(listItems[i]);
   }
 });
 
-listTask.addEventListener ('click', function(event) {
+listTask.addEventListener('click', function (event) {
   if (document.querySelector('.selected') !== null) {
     document.querySelector('.selected').style.backgroundColor = 'white';
     document.querySelector('.selected').classList.remove('selected');
@@ -29,10 +29,10 @@ listTask.addEventListener ('click', function(event) {
 });
 
 const removeSelected = document.getElementById('remover-selecionado');
-removeSelected.addEventListener('click', function() {
+removeSelected.addEventListener('click', function () {
   const remove = document.querySelector('.selected');
   listTask.removeChild(remove);
-})
+});
 
 listTask.addEventListener('dblclick', function (event) {
   if (event.target.classList.contains('completed')) {
@@ -42,21 +42,16 @@ listTask.addEventListener('dblclick', function (event) {
   }
 });
 
-removeFinished.addEventListener ('click', function() {
+removeFinished.addEventListener('click', function () {
   const listItems = document.querySelectorAll('ol .completed');
-  for (let j = 0; j < listItems.length; j += 1 ) {
-      listTask.removeChild(listItems[j]);
-    }
+  for (let j = 0; j < listItems.length; j += 1) {
+    listTask.removeChild(listItems[j]);
   }
-);
-
-if (typeof Storage !== 'undefine') {
-  listTask.innerHTML = localStorage.listTask;
-}
-
-const salveStorage = document.getElementById('salvar-tarefas');
-salveStorage.addEventListener('click', function(){
-  localStorage.listTask = document.getElementById('lista-tarefas').innerHTML;
 });
 
+listTask.innerHTML = localStorage.listTask;
 
+const salveStorage = document.getElementById('salvar-tarefas');
+salveStorage.addEventListener('click', function () {
+  localStorage.listTask = document.getElementById('lista-tarefas').innerHTML;
+});
