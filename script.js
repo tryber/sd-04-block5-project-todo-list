fcb4109e02fa4070e6db8b9fb64700400e9fa2f6
danilossousa <danilosantossousa@gmail.com>
2020-04-13 21:40:43 -0300

    v0.12.0.11

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
 let i;
 let j;
+let indClass = [];
+let removeMouseOut = false;
 const butAddTask = document.getElementById('criar-tarefa');
 const olTasks = document.getElementById('lista-tarefas');
 const inptasks = document.getElementById('texto-tarefa');
 const butDelTasks = document.getElementById('apaga-tudo');
 const butDelFinish = document.getElementById('remover-finalizados');
 const butSaveTasks = document.getElementById('salvar-tarefas');
-let indClass = [];
+const butUp = document.getElementById('mover-cima');
+const butDown = document.getElementById('mover-baixo');
+
+function captureSelected() {
+  let selected = null;
+  
+  for (i = 0; i < olTasks.children.length; i += 1) {
+    if (olTasks.children[i].style.backgroundColor === 'rgb(128, 128, 128)') {
+      selected = olTasks.children[i];
+    }
+  }
+
+  return selected;
+}
+
+function liMouseOver(e) {
+  e.target.style.backgroundColor = 'rgb(128 , 128 , 128)';
+}
+
+function liMouseOut(e) {
+  e.target.style.backgroundColor = 'rgb(103 , 230 , 141)';
+}
+
+function liDbclick(e) {
+  if (e.target.className) {
+    e.target.className = '';
+  } else {
+      e.target.className = 'completed';
+    }
+}
+
+function liClick(e) {
+  if (removeMouseOut === false) {
+    e.target.removeEventListener('mouseout', liMouseOut);
+    removeMouseOut = true;
+  } else {
+    e.target.addEventListener('mouseout', liMouseOut);
+    removeMouseOut = false;
+  }
+}
 
 function addEvents(elm) {
   elm.style.cursor = 'pointer';
-  elm.addEventListener('mouseover', function (e) { e.target.style.backgroundColor = 'rgb(128 , 128 , 128)'; });
-  elm.addEventListener('mouseout', function (e) { e.target.style.backgroundColor = 'rgb(103 , 230 , 141)'; });
-  elm.addEventListener('dblclick', function (e) {
-    if (e.target.className) {
-      e.target.className = '';
-    } else {
-      e.target.className = 'completed';
-    }
-  });
+  
+  elm.addEventListener('mouseover', liMouseOver);
+  elm.addEventListener('mouseout', liMouseOut);
+  elm.addEventListener('dblclick', liDbclick);
+  elm.addEventListener('click', liClick);
 }
 
 function addClass(elm, idc) {
@@ -93,4 +130,24 @@ butSaveTasks.addEventListener('click', function () {
   }
 
   localStorage.setItem('indClass', JSON.stringify(indClass));
+});
+
+butUp.addEventListener('click', function () {
+  const elm = captureSelected();
+  const elmDad = elm.parentNode;
+  
+  if (elm !== elmDad.firstChild) {
+    elmDad.insertBefore(elm, elm.previousSibling);
+  }
+});
+
+butDown.addEventListener('click', function () {
+  let elm = captureSelected();
+  const elmDad = elm.parentNode;
+
+  if (elm !== elmDad.lastChild) {
+    elm = captureSelected().nextSibling;
+
+    elmDad.insertBefore(elm, elm.previousSibling);
+  }
 });
