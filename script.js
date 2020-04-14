f1ae6f860cfd2fdb9c20489e559e79900175d03e
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-04-13 23:57:48 -0300

    feature/completed - remove all - remove completed

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Query selectors
 const addTaskBtn = document.querySelector('.criar-tarefa');
+const deleteAllBtn = document.querySelector('.apaga-tudo');
+const deleteCompletedBtn = document.querySelector('.remover-finalizados');
 const taskList = document.querySelector('.lista-tarefas');
 const text = document.querySelector('.texto-tarefa');
 
+// Create task element
 function createTask() {
   const task = document.createElement('li');
   task.classList.add('task-li');
@@ -12,10 +15,27 @@ function createTask() {
     tasks.forEach((item) => { if (item !== event.target) item.classList.remove('selected'); });
     event.target.classList.toggle('selected');
   });
+  task.addEventListener('dblclick', () => {
+    event.target.classList.toggle('completed');
+  });
   return task;
 }
 
+// Create task event listener
 addTaskBtn.addEventListener('click', () => {
   if (text.value !== '') taskList.appendChild(createTask());
   text.value = '';
+});
+
+// Delete all button
+deleteAllBtn.addEventListener('click', () => {
+  while (taskList.firstChild) taskList.removeChild(taskList.firstChild);
+});
+
+// Delete completed button
+deleteCompletedBtn.addEventListener('click', () => {
+  const completed = document.querySelectorAll('.completed');
+  completed.forEach((item) => {
+    item.parentNode.removeChild(item);
+  });
 });
