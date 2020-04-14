560fadaed34ed671ecff2c21c6934b25b1e28cf9
cacobribeiro <cacobribeiro@gmail.com>
2020-04-14 16:34:45 -0300

    bottons add task and delete all are fixed

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
-let addTarefa = document.getElementById("#texto-tarefa");
-let createOrdenada = document.createElement("li");
-let listaOrdenada = document.querySelector("ol");
-let lista = document.querySelectorAll("li");
+const criarTarefa = document.getElementById("criar-tarefa");
+const tarefa = document.getElementById("texto-tarefa")
+const ol = document.getElementById("lista-tarefas")
+const deleteTarefas = document.getElementById('apaga-tudo');
+const li = document.querySelectorAll("li");
 
 
-function createLi(){
-    listaOrdenada.appendChild(createOrdenada);
-    for( let i = 0; i < listaOrdenada.length; i++ ) {
-        addTarefa = addEventListener('input', function(){
-        lista[lista.length].innerHTML = 'addTarefa.value';
-    }
-)}
+criarTarefa.addEventListener("click", addLi);
+function addLi() {
+    let li = document.createElement('li');
+    li.innerHTML = tarefa.value;
+    ol.appendChild(li);
 }
+
+deleteTarefas.addEventListener('click', deleteAll);
+function deleteAll() {
+    ol.innerHTML = "";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let addTarefa = document.getElementById("#texto-tarefa");
+// let listaOrdenada = document.querySelector("ol");
+// let lista = document.querySelectorAll("li");
+// const buttonInsert = document.getElementById("criar-tarefa");
+
+
+// function createLi(tarefa){
+//     let li = document.createElement("li");
+//     li.innerHTML = tarefa;
+//     listaOrdenada.appendChild(li);   
+// }
+
+// buttonInsert.addEventListener('click', function() {
+//     createLi(addTarefa.value);
+// })
