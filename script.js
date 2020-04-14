ffe32e5b06b8b725a439bb3910a4fa1c83dace25
cacobribeiro <cacobribeiro@gmail.com>
2020-04-14 18:30:54 -0300

    Buttons com maozinha e selecionadas

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const ol = document.getElementById("lista-tarefas")
 const deleteTarefas = document.getElementById('apaga-tudo');
 const li = document.querySelectorAll("li");
 const item = document.getElementsByClassName("item")
+const buttons = document.getElementsByTagName('button');
 
+// Cursor em forma de mãozinha
+for (let i = 0; i < buttons.length; i+=1){
+buttons[i].addEventListener('mouseover', function(){
+event.target.style.cursor = 'pointer';
+})
+}
 
-
+// criar e adicionar li's ao OL.
 criarTarefa.addEventListener("click", addLi);
 function addLi() {
     let li = document.createElement('li');
@@ -15,12 +22,13 @@ function addLi() {
     ol.appendChild(li);
     tarefa.value = '';
 }
-
+// Deleta tudo de OL. *Apaga todas LI*//
 deleteTarefas.addEventListener('click', deleteAll);
 function deleteAll() {
     ol.innerHTML = "";
 }
 
+//Seleciona e em breve, Duplo click.
 ol.addEventListener('click', start);
 function start() {
 for( let i = 0; i < item.length; i+= 1) {
@@ -31,21 +39,3 @@ for( let i = 0; i < item.length; i+= 1) {
         item[i].classList.add('selected');
     })
 }}
-
-
-
-// let addTarefa = document.getElementById("#texto-tarefa");
-// let listaOrdenada = document.querySelector("ol");
-// let lista = document.querySelectorAll("li");
-// const buttonInsert = document.getElementById("criar-tarefa");
-
-
-// function createLi(tarefa){
-//     let li = document.createElement("li");
-//     li.innerHTML = tarefa;
-//     listaOrdenada.appendChild(li);   
-// }
-
-// buttonInsert.addEventListener('click', function() {
-//     createLi(addTarefa.value);
-// })
