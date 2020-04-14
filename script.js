f37ccf9eae76f21ace7ae9ee71ad7016f7893de3
TerezaCarvalho <terezasimoes@gmail.com>
2020-04-14 19:49:52 -0300

    classes

--- script.js
+++ script.js
@@ -7,9 +7,21 @@ let btn_remove_lista = document.getElementById('apaga-tudo');
 
 window.onload = function(){
 
+    if (typeof (Storage) !== "undefined") {  // *** load 
+        olNode.innerHTML = localStorage.lista; // ol recebe itens salvos 'lista'
+    }
+
+      // PARA SALVAR LISTA ***
+    let btn_salva_tarefa = document.getElementById('salvar-tarefas');
+    if (olNode.innerHTML == '') {
+        alert('Não existe tarefas para salvar');
+    }else{ // se tiver algo para salvar chama o evento
+        btn_salva_tarefa.addEventListener('click',function(){
+            localStorage.setItem('lista', olNode.innerHTML); // salvando os itens dentro da 'lista'
+        })
+    }
+
     
-
-
     // CRIAR TAREFA
     function addTarefa(){
         let liNode = document.createElement('li'); 
@@ -19,27 +31,26 @@ window.onload = function(){
         textoDigitado.value = '';
         textoDigitado.focus();
     
-        // ADICIONAR CLASSE #
-    liNode.addEventListener('click', function(){//#
-        //liNode.classList.add('selected');// li recebe a classe selected do css, PRECISA função
-        trocarClasse(liNode,'selected');
-        
-    })
-
-    liNode.addEventListener('dblclick', function(){//#
+        // ADICIONAR e REMOVE CLASSE SELECTED e COMPLETED#
+   // liNode.addEventListener('click', function(){//#
+        // 1-liNode.classList.add('selected');// li recebe a classe selected do css, PRECISA função
+       // trocarClasse(liNode,'selected');  
+   // })
+   // liNode.addEventListener('dblclick', function(){//#
        // liNode.classList.add('completed');// li recebe a classe completed do css PRECISA função
-       trocarClasse(liNode,'completed');
-    })
+      // trocarClasse(liNode,'completed');
+   // })
 
-    function trocarClasse(element, classe){ //#  parâmetros = o que as linhas 17/25 tem em comum.
-        if( element.classList.contains(classe)== false){ //1-qnd elem não contem a classe
+   // function trocarClasse(element, classe){ //#  parâmetros = o que as linhas 17/25 tem em comum.
+        //if( element.classList.contains(classe)== false){ //1-qnd elem não contem a classe
              //sintax = node.contains() e retorna true ou false     
-            element.classList.add(classe);//2- classe adicionada ao elem.
-        }else{
-            element.classList.remove(classe);//3-senão, classe removida do elem.
-        }
-       
-    }
+           // element.classList.add(classe);//2- classe adicionada ao elem.
+       // }else{
+           // element.classList.remove(classe);//3-senão, classe removida do elem.
+       // }
+   // }
+
+
     // REMOVER FINALIZADOS E SELECIONADOS ##
     let btn_remove_finaliz = document.getElementById('remover-finalizados');
     let btn_remove_selec = document.getElementById('remover-selecionado');
@@ -65,28 +76,27 @@ window.onload = function(){
         }
     }
 
-
     }// fim addTarefa
 
-    
-    if (typeof (Storage) !== "undefined") {  // *** load 
-        olNode.innerHTML = localStorage.lista; // lista 
-      }
-
-      // PARA SALVAR LISTA ***
-    let btn_salva_tarefa = document.getElementById('salvar-tarefas');
-    if (olNode.innerHTML == '') {
-        alert('Não existe tarefas para salvar');
-    }else{ // se tiver algo para salvar chama o evento
-        btn_salva_tarefa.addEventListener('click',function(){
-            localStorage.setItem('lista', olNode.innerHTML); // salvando os itens dentro da 'lista'
-        })
+    // ADICIONAR CLASSE SELECTED
+    // Ao clicar uma vez sobre a li add a classe
+    olNode.addEventListener('click', function (el) {
+    // verifica se a classe ja está no elemento clicado
+        if (document.querySelector('.selected') == null) {
+        el.target.classList.add('selected');
+        //console.log(el);
+        // console.log(el.target);
+        }   
+    });
+      // Ao clicar duas vezes sobre a li a classe é add ou removida
+    olNode.addEventListener('dblclick', function (el) {
+    if (el.target.classList.contains('completed') === true) {
+      el.target.classList.remove('completed');
+    } else {
+      el.target.classList.add('completed');
     }
-   
+  });
     
-
-    
-
 
     // APAGA-TUDO ou REMOVENDO ITENS DA LISTA
      function limpaLista(){
@@ -94,10 +104,6 @@ window.onload = function(){
     }
 
     
-
-  
-  
-
 
     // adicionar tarefa com tecla enter
     textoDigitado.addEventListener('keyup', function(e){
