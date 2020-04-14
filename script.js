4a7340bd6ffa6522df1488fc314363d23185fa05
Josias Viveiro <josiasviveiro@gmail.com>
2020-04-14 18:45:54 -0300

    css, js - requisito 9

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ window.onload = function () {
   let btnCreateTask = document.getElementById('criar-tarefa');
   let taskList = document.getElementById('lista-tarefas');
 
+  function clickTask(event) {
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+  }
+
+  function completedTask(event) {
+    if (event.target.className === 'completed') {
+      event.target.className = '';
+    } else {
+      event.target.className = 'completed';
+    }
+  }
+
   function createTask() {
     let newTask = document.createElement('li');
     newTask.innerText = textTask.value;
     newTask.addEventListener('click', clickTask);
+    newTask.addEventListener('dblclick', completedTask);
     taskList.appendChild(newTask);
     textTask.value = '';
-  }
-
-  function clickTask(event) {
-    event.target.style.backgroundColor = 'rgb(128,128,128)';
   }
 
   btnCreateTask.addEventListener('click', createTask);
