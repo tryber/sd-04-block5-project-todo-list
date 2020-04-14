b178fa13fd70d7314910e10229c158d57b013279
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-04-13 23:23:08 -0300

    feature/add task remake

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ const addTaskBtn = document.querySelector('.criar-tarefa');
 const taskList = document.querySelector('.lista-tarefas');
 const text = document.querySelector('.texto-tarefa');
 
-// Create task function
 function createTask() {
   const task = document.createElement('li');
+  task.classList.add('task-li');
   task.innerHTML = text.value;
+  task.addEventListener('click', () => {
+    const tasks = document.querySelectorAll('.task-li');
+    tasks.forEach((item) => { if (item !== event.target) item.classList.remove('selected'); });
+    event.target.classList.toggle('selected');
+  });
   return task;
 }
 
-// Add task event listener
 addTaskBtn.addEventListener('click', () => {
-  taskList.appendChild(createTask());
+  if (text.value !== '') taskList.appendChild(createTask());
   text.value = '';
 });
