c6f7ded9073280131d4438be33bb34d23513c424
Lucas Barbosa Rosa <lucaslucas2008@gmail.com>
2020-04-13 23:52:41 -0300

    Changes in  requisition 9

--- script.js
+++ script.js
@@ -1,10 +1,60 @@
 const lista = document.getElementById("lista-tarefas");
 const buttonAdd = document.getElementById("criar-tarefa");
 const inputText = document.getElementById("texto-tarefa");
+let ultimoMarcado;
 
 function Seleciona(event)
 {
-    event.target.className = "selected";
+    if(event.target.className != "selected" && event.target.className != "selected completed" && event.target.className != "completed")
+    {
+        event.target.className = "selected";
+        if(ultimoMarcado)
+        {
+            console.log("pas 3");
+            ultimoMarcado.classList.remove('selected');
+            ultimoMarcado = event.target;
+        }
+        else
+        {
+            console.log("pas 2");
+            ultimoMarcado = event.target;
+        }
+    }
+    else if(event.target.className == "selected completed")
+    {
+        console.log("pas 4");
+        if(ultimoMarcado.className != event.target.className)
+        {
+            ultimoMarcado.classList.remove('selected');
+            ultimoMarcado = event.target;
+        }
+    }
+    else if(event.target.className == "completed")
+    {
+        
+        event.target.className = "selected completed"
+        if(ultimoMarcado.className == "selected completed" || ultimoMarcado.className == "selected")
+        {
+            console.log("pas 5");
+            ultimoMarcado.classList.remove('selected');
+            ultimoMarcado = event.target;
+        }
+    }
+}
+
+function Completou(event)
+{
+    if(event.target.className != "selected completed" && event.target.className != "completed")
+    {
+        event.target.className = "selected completed";
+        Seleciona(event);
+    }
+    else if(event.target.className == "selected completed" || event.target.className == "completed")
+    {
+        event.target.className = "selected";
+        Seleciona(event);
+    }
+
 }
 
 function AddElemento()
@@ -13,6 +63,7 @@ function AddElemento()
     const informacao = inputText.value;
     elemento.innerHTML = informacao;
     //elemento.setAttribute('class', 'textos');
+    elemento.addEventListener('dblclick', function (event) { Completou(event); });
     elemento.addEventListener('click', function (event) { Seleciona(event); });
     lista.appendChild(elemento);
     inputText.value = '';
