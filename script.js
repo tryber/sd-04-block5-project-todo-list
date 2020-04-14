e54a4162ed13f6704ae6edfc6b2deb1824a0860b
Victor L <victor.lustosacp@gmail.com>
2020-04-14 17:27:35 -0300

    teste dos botoes

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-let button = document.getElementById('criar-tarefa');
-let ltarefa = document.getElementById('lista-tarefas');
-let txt = document.getElementById('texto-tarefa');
-button.addEventListener( 'click' ,  function() {
+let botao = document.getElementById('criar-tarefa');
+let lista = document.getElementById('lista-tarefas');
+let texto = document.getElementById('texto-tarefa');
+botao.addEventListener( 'click' ,  function() {
   let criar = document.createElement('LI');
-  criar.innerHTML = txt.value;
+  criar.innerHTML = texto.value;
   criar.className = 'cursor';
-  txt.value ='';
-  ltarefa.appendChild(criar);
+  texto.value ='';
+  lista.appendChild(criar);
+}); 
+lista.addEventListener('click', function(event) {
+  event.target.classList.add('point');
 });
+lista.addEventListener('dblclick', function(event) {
+    event.target.classList.toggle('completed');
+  })
+  let apagar = document.getElementById('apaga-tudo');
+  let apagados = document.getElementsByTagName('li');
+  apagar.addEventListener('click',function() {
+    var j = apagados.length;
+    var i = 0 ;
+      for ( i = 1; i <= j ; i++ ){
+        lista.removeChild(apagados[j - i]);
+        console.log(apagados.length);
+      }
+  })
