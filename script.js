a9aab31b4e189988a0d8d64e52388c4fffdd9f18
Tiago Emanuel <trsilva284@gmail.com>
2020-04-14 18:05:07 -0300

    inserindo botoes

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 let criartarefa = document.querySelector("#criar-tarefa");
 console.log(criartarefa);
 
-let listaDeTarefas = document.querySelector(".lista-tarefas");
+let listaDeTarefas = document.querySelector("#lista-tarefas");
+console.log(listaDeTarefas);
 
 criartarefa.addEventListener("click", function(event){
     let form = document.querySelector("#texto-tarefa");  
     if(form.value == "")
     {
-        return;
+       return;
     } 
     let tarefaLista = form.value;
     montaLista(tarefaLista);
@@ -26,6 +27,27 @@ listaDeTarefas.addEventListener("click", function(e){
 listaDeTarefas.addEventListener("mouseover", function(e){
     e.target.classList.toggle("pointer");
  });
+
+ //Apaga toda a lista
+let apagaLista = document.querySelector("#apaga-tudo");
+apagaLista.addEventListener("click", function(e){
+    while (listaDeTarefas.firstChild) {
+        listaDeTarefas.removeChild(listaDeTarefas.firstChild);
+      }
+});
+
+//Remove itens selecionados   
+let removeSelecionado = document.querySelector("#remover-selecionado");
+removeSelecionado.addEventListener("click", function(e){
+    //Obtêm todas as tarefas - li 
+   let tarefasSelecionadas = document.getElementsByTagName("li");      
+   for(var i =0; i < tarefasSelecionadas.length; i++)
+     //Verifica se a tarefa está selecionada
+     if( tarefasSelecionadas[i].classList.contains("alteracor")){
+         //Remove tarefa selecionada
+        listaDeTarefas.removeChild(tarefasSelecionadas[i]);        
+   }    
+});
 
 //Adiciona a tarefa na lista com a classe (lista-tarefas)
 function montaLista(tarefaLista){
