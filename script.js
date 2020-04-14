6a67ea23b2d13064df2c7fdde8c7442050d4840d
juniomelos <juniomelos@gmail.com>
2020-04-14 15:28:18 -0300

    requisite 11

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 const taskText = document.getElementById('texto-tarefa');
 const buttonCreateTask = document.getElementById('criar-tarefa');
-const listTask = document.getElementById('lista-tarefas')
+const listTask = document.getElementById('lista-tarefas');
 const buttonDeleteList = document.getElementById('apaga-tudo');
-const finished = document.getElementsByClassName('finished');
+const removeFinished = document.getElementById('remover-finalizados');
 
-buttonCreateTask.addEventListener('click', function(){
+buttonCreateTask.addEventListener('click', function () {
   let valueTaskText = taskText.value;
   let li = document.createElement('li');
   li.classList.add('listTask');
   listTask.appendChild(li).innerHTML = valueTaskText;
-  taskText.value = ''
-})
-
-buttonDeleteList.addEventListener('click', function(){
-  let listItems = document.querySelectorAll('.listTask');
-  for (i = 0; i < listItems.length; i += 1){
-    listTask.removeChild(listItems[i]);
-  }
-})
-
-document.body.addEventListener('click', function(event){
-  if (event.target.className == 'listTask'){
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  };
+  taskText.value = '';
 });
 
-listTask.addEventListener('dblclick', function(event){
-  if (event.target.classList.contains('completed')){
-    event.target.classList.remove ('completed');
+buttonDeleteList.addEventListener('click', function () {
+  const listItems = document.querySelectorAll('.listTask');
+  for ( let i = 0; i < listItems.length; i += 1) {
+    listTask.removeChild(listItems[i]);
+  }
+});
+
+document.body.addEventListener('click', function (event) {
+  if (event.target.className == 'listTask') {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+});
+
+listTask.addEventListener('dblclick', function (event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
   } else {
     event.target.classList.add('completed');
   }
 });
 
+removeFinished.addEventListener ('click', function() {
+  const concluidas = document.querySelectorAll('.completed');
+  for (let j = 0; j < concluidas.length; j += 1 ) {
+      listTask.removeChild(concluidas[j]);
+    }
+  }
+)
