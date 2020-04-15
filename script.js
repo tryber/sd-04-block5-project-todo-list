613f90216ba791c591ae8c5b7d5a7bb8dff834ca
Josias Viveiro <josiasviveiro@gmail.com>
2020-04-14 21:02:20 -0300

    html, js - requisitos 10 e 11

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ window.onload = function () {
   let textTask = document.getElementById('texto-tarefa');
   let btnCreateTask = document.getElementById('criar-tarefa');
   let taskList = document.getElementById('lista-tarefas');
+  let btnCleanList = document.getElementById('apaga-tudo');
+  let btnRemoveDone = document.getElementById('remover-finalizados');
 
   function clickTask(event) {
     event.target.style.backgroundColor = 'rgb(128,128,128)';
@@ -15,6 +17,21 @@ window.onload = function () {
     }
   }
 
+  function cleanList() {
+    let listSize = taskList.childNodes.length;
+    for (let i = 0; i < listSize; i += 1) {
+      taskList.removeChild(taskList.lastChild);
+    }
+  }
+
+  function cleanDone() {
+    let taskDone = document.getElementsByClassName('completed');
+    let taskDoneList = taskDone.length;
+    for (let i = 0; i < taskDoneList; i += 1) {
+        taskDone[0].remove();
+    }
+  }
+
   function createTask() {
     let newTask = document.createElement('li');
     newTask.innerText = textTask.value;
@@ -25,4 +42,6 @@ window.onload = function () {
   }
 
   btnCreateTask.addEventListener('click', createTask);
+  btnCleanList.addEventListener('click', cleanList);
+  btnRemoveDone.addEventListener('click', cleanDone);
 };
