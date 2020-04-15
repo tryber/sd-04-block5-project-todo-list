6dbf0c40abf2ecba8ccb718124d7d6882eb11ef1
djonata-dias <diasdjonata@gmail.com>
2020-04-14 23:22:28 -0300

    lista funcionando, item 1 à 6 feitos

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 window.onload = function () {
-    
+    let taskList = document.getElementById('lista-tarefas')
+    let taskText = document.getElementById('texto-tarefa')
+    let createButton = document.getElementById('criar-tarefa')
+
+    createButton.addEventListener('click', function() {
+        let tarefa = document.createElement('li')
+        tarefa.innerHTML = taskText.value
+        taskList.appendChild(tarefa)
+    })
 }
