da13e530150ce2f94b8f28fe93df1b02d79feece
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-04-15 17:11:40 -0300

    Created a function to scratch the todo when double clicked

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ const addBtn = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const creatTask = document.createElement('li');
 
-// Change BG of todo to Gray
-function changeBG(event) {
+// Change BG of todo clicked to Gray
+function changeBG() {
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+// Scratch the todo when double clicked
+function finishedToDo() {
+  if (event.target.className === 'completed' ) {
+    event.target.className.remove('completed');
+  } else {
+    event.target.className = 'complete';
+  }
 }
 
 // Add a Task
@@ -13,13 +22,11 @@ function addToDo() {
   addBtn.addEventListener('click', function () {
     const task = document.createElement('li');
     task.innerHTML = taskTxt.value;
-    task.addEventListener('click', changeBG)
+    task.addEventListener('dblclick', finishedToDo);
+    task.addEventListener('click', changeBG);
     taskList.appendChild(task);
     taskTxt.value = '';
   })
 }
 
 addToDo();
-
-
-
