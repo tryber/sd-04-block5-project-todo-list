626f3b249643246aaa316864b8bf5122c35eea0c
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-04-15 15:55:55 -0300

    Added the function to color background of todo

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ const addBtn = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const creatTask = document.createElement('li');
 
-addBtn.addEventListener('click', function () {
-  const task = document.createElement('li');
-  task.innerHTML = taskTxt.value;
-  taskList.appendChild(task);
-  taskTxt.value = '';
-})
+// Change BG of todo to Gray
+function changeBG(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+// Add a Task
+function addToDo() {
+  addBtn.addEventListener('click', function () {
+    const task = document.createElement('li');
+    task.innerHTML = taskTxt.value;
+    task.addEventListener('click', changeBG)
+    taskList.appendChild(task);
+    taskTxt.value = '';
+  })
+}
+
+addToDo();
+
+
+
