29e24edef45e551d348a05d1c42354591a6213b4
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-04-15 14:15:15 -0300

    Fix the clean input after add a task

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const taskTxt = document.getElementById('texto-tarefa');
+const taskTxt = document.querySelector('#texto-tarefa');
 const addBtn = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const creatTask = document.createElement('li');
@@ -7,5 +7,5 @@ addBtn.addEventListener('click', function () {
   const task = document.createElement('li');
   task.innerHTML = taskTxt.value;
   taskList.appendChild(task);
-  task.value = '';
+  taskTxt.value = '';
 })
