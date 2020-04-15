69950165fecb9ac8393ca1b67df76bcdc8ed4fea
deboracosilveira <deboracosilveira@gmail.com>
2020-04-15 12:46:36 -0300

    codeclimate

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // Variables
-let newTask = document.getElementById("texto-tarefa");
-let createTask = document.getElementById("criar-tarefa");
-let deleteAll = document.getElementById("apaga-tudo");
-let deleteCompletedTasks = document.getElementById("remover-finalizados");
-let list = document.getElementById("lista-tarefas")
-let saveTasks = document.getElementById("salvar-tarefas")
-let buttons = document.querySelectorAll("button");
-let deleteSelectedTask = document.getElementById("remover-selecionado");
-let moveUp = document.getElementById("mover-cima");
-let moveDown = document.getElementById("mover-baixo");
+const newTask = document.getElementById('texto-tarefa');
+const createTask = document.getElementById('criar-tarefa');
+const deleteAll = document.getElementById('apaga-tudo');
+const deleteCompletedTasks = document.getElementById('remover-finalizados');
+const list = document.getElementById('lista-tarefas');
+const saveTasks = document.getElementById('salvar-tarefas');
+const buttons = document.querySelectorAll('button');
+const deleteSelectedTask = document.getElementById('remover-selecionado');
+const moveUp = document.getElementById('mover-cima');
+const moveDown = document.getElementById('mover-baixo');
 
 // New item is created when the user press the button
-createTask.addEventListener("click", addItem);
 function addItem() {
-  if(newTask.value !== "") { //Do not accept an empty taks
-    let task = document.createElement("li");
+  if (newTask.value !== '') { // Do not accept an empty taks
+    const task = document.createElement('li');
     task.innerText = newTask.value;
-    list.appendChild(task)
-    newTask.value = "";
+    list.appendChild(task);
+    newTask.value = '';
   }
 }
+createTask.addEventListener('click', addItem);
 // Select task and change color background
 list.addEventListener('click', (event) => {
   if (document.querySelector('.selected') !== null) {
@@ -30,126 +30,121 @@ list.addEventListener('click', (event) => {
   event.target.classList.add('selected');
 });
 // Change buttons cursor type
-for( i = 0; i < buttons.length; i+=1) {
-  buttons[i].style.cursor = "pointer";
-};
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].style.cursor = 'pointer';
+}
 // Setting a completed task
-list.addEventListener("dblclick", (event) => {
-  if(event.target.classList.contains("completed")) {
-    event.target.classList.remove("completed")
+list.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
   } else {
-    event.target.classList.add("completed")
+    event.target.classList.add('completed');
   }
-})
+});
 // Delete completed tasks
-deleteCompletedTasks.addEventListener("click", deleteCompleted);
 function deleteCompleted() {
-  let items = document.querySelectorAll(".completed");
-  for(i = 0; i < items.length; i+=1) {
-    items[i].parentNode.removeChild(items[i]); 
+  const items = document.querySelectorAll('.completed');
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].parentNode.removeChild(items[i]);
   }
 }
+deleteCompletedTasks.addEventListener('click', deleteCompleted);
 // Delete all tasks
-deleteAll.addEventListener("click", deleteAllTasks)
 function deleteAllTasks() {
-  let items = document.querySelector("ol")
-  items.innerHTML = "";
+  const items = document.querySelector('ol');
+  items.innerHTML = '';
 }
+deleteAll.addEventListener('click', deleteAllTasks);
 // Delete selected item
-deleteSelectedTask.addEventListener("click", function() {
-  let item = document.querySelector(".selected");
-  item.parentNode.removeChild(item)
-})
+deleteSelectedTask.addEventListener('click', function () {
+  const item = document.querySelector('.selected');
+  item.parentNode.removeChild(item);
+});
 // Set Local storage
-saveTasks.addEventListener("click", setLocalStorage);
 function setLocalStorage() {
   localStorage.clear();
-  let taskList = document.querySelectorAll("li");
-    for (i = 0; i < taskList.length; i+=1) {
-      let className = taskList[i].className;
-      let task = taskList[i].innerHTML;
-      let taskIndex = `Task ${i}`;
-      let classIndex = `Class ${i}`;
-      localStorage.setItem(taskIndex, task)
-      localStorage.setItem(classIndex, className)
-    }
-    localStorage.setItem("numberOfItems", taskList.length)
+  const taskList = document.querySelectorAll('li');
+  for (let i = 0; i < taskList.length; i += 1) {
+    const className = taskList[i].className;
+    const task = taskList[i].innerHTML;
+    const taskIndex = `Task ${i}`;
+    const classIndex = `Class ${i}`;
+    localStorage.setItem(taskIndex, task);
+    localStorage.setItem(classIndex, className);
   }
-// Get local storage when load page
-window.onload = function() {
-  let index = localStorage.getItem("numberOfItems");
-  for( let i = 0; i < index; i+=1) {
-    let item = document.createElement("li")
-    item.className = localStorage.getItem(`Class ${i}`)
-    list.appendChild(item).innerHTML = localStorage.getItem(`Task ${i}`)//load list content
-  }  
+  localStorage.setItem('numberOfItems', taskList.length);
 }
-// Move item up
-moveUp.addEventListener("click", function() {
-  let selectedTask = document.querySelector(".selected");
-  let tasks = document.querySelectorAll("li");// Alert that is not possible to move up more
-  if(selectedTask !== tasks[0]) {
-    let previousTask = selectedTask.previousElementSibling;
-    let previousTaskInnerHTML = previousTask.innerHTML;
-  
-    previousTask.innerText = selectedTask.innerHTML;// Set selected task to previous position
-    previousTask.classList.add("selected")// Keep style on selected task 
-    previousTask.style.backgroundColor = "rgb(128,128,128)"
-  
-    if(selectedTask.classList.contains("completed")) {// Bring style when move 
-      if(!previousTask.classList.contains("completed")) {
-        previousTask.classList.add("completed");
-        selectedTask.classList.remove("completed");
-      } else {
-        previousTask.classList.add("completed");
-        selectedTask.classList.add("completed");
-      }
-    } else {
-      if(previousTask.classList.contains("completed")) {
-        previousTask.classList.remove("completed");
-        selectedTask.classList.add("completed");
-      }
-    }
-    selectedTask.classList.remove("selected");// Take out style from previous task 
-    selectedTask.style.backgroundColor = "";
-  
-    selectedTask.innerHTML = previousTaskInnerHTML;//Set previous task to next position
-  } else {
-    alert("A tarefa já está na primeira posição.")
+saveTasks.addEventListener('click', setLocalStorage);
+// Get local storage when load page
+window.onload = function () {
+  const index = localStorage.getItem('numberOfItems');
+  for (let i = 0; i < index; i += 1) {
+    const item = document.createElement('li');
+    item.className = localStorage.getItem(`Class ${i}`);
+    list.appendChild(item).innerHTML = localStorage.getItem(`Task ${i}`);// Load list content
   }
-})
-// Move item down
-moveDown.addEventListener("click", function() {
-  let selectedTask = document.querySelector(".selected");
+};
+// Move item up
+moveUp.addEventListener('click', function () {
+  const selectedTask = document.querySelector('.selected');
+  const tasks = document.querySelectorAll('li');// Alert that is not possible to move up more
+  if (selectedTask !== tasks[0]) {
+    const previousTask = selectedTask.previousElementSibling;
+    const previousTaskInnerHTML = previousTask.innerHTML;
+    previousTask.innerText = selectedTask.innerHTML;// Set selected task to previous position
+    previousTask.classList.add('selected');// Keep style on selected task
+    previousTask.style.backgroundColor = 'rgb(128,128,128)';
 
-  let tasks = document.querySelectorAll("li");// Alert that is not possible to move down more
-  if(selectedTask !== tasks[tasks.length - 1]) {
-    let nextTask = selectedTask.nextElementSibling;
-    let nextTaskInnerHTML = nextTask.innerHTML;
-  
-    nextTask.innerText = selectedTask.innerHTML;// Set selected task to next position
-    nextTask.classList.add("selected")// Keep style on selected task 
-    nextTask.style.backgroundColor = "rgb(128,128,128)"
-  
-    if(selectedTask.classList.contains("completed")) {// Bring style when move 
-      if(!nextTask.classList.contains("completed")) {
-        nextTask.classList.add("completed");
-        selectedTask.classList.remove("completed");
+    if (selectedTask.classList.contains('completed')) { // Bring style when move
+      if (!previousTask.classList.contains('completed')) {
+        previousTask.classList.add('completed');
+        selectedTask.classList.remove('completed');
       } else {
-        nextTask.classList.add("completed");
-        nextTask.classList.add("completed");
+        previousTask.classList.add('completed');
+        selectedTask.classList.add('completed');
       }
-    } else {
-      if(nextTask.classList.contains("completed")) {
-        nextTask.classList.remove("completed");
-        selectedTask.classList.add("completed");
-      }
+    } else if (previousTask.classList.contains('completed')) {
+      previousTask.classList.remove('completed');
+      selectedTask.classList.add('completed');
     }
-    selectedTask.classList.remove("selected");// Take out style from previous task 
-    selectedTask.style.backgroundColor = "";
-  
-    selectedTask.innerHTML = nextTaskInnerHTML;//Set previous task to next position
+    selectedTask.classList.remove('selected');// Take out style from previous task
+    selectedTask.style.backgroundColor = '';
+
+    selectedTask.innerHTML = previousTaskInnerHTML;// Set previous task to next position
   } else {
-    alert("A tarefa já está na última posição.")
-  }  
-})
+    alert('A tarefa já está na primeira posição.');
+  }
+});
+// Move item down
+moveDown.addEventListener('click', function () {
+  const selectedTask = document.querySelector('.selected');
+
+  const tasks = document.querySelectorAll('li');// Alert that is not possible to move down more
+  if (selectedTask !== tasks[tasks.length - 1]) {
+    const nextTask = selectedTask.nextElementSibling;
+    const nextTaskInnerHTML = nextTask.innerHTML;
+
+    nextTask.innerText = selectedTask.innerHTML;// Set selected task to next position
+    nextTask.classList.add('selected');// Keep style on selected task
+    nextTask.style.backgroundColor = 'rgb(128,128,128)';
+
+    if (selectedTask.classList.contains('completed')) { // Bring style when move
+      if (!nextTask.classList.contains('completed')) {
+        nextTask.classList.add('completed');
+        selectedTask.classList.remove('completed');
+      } else {
+        nextTask.classList.add('completed');
+        nextTask.classList.add('completed');
+      }
+    } else if (nextTask.classList.contains('completed')) {
+      nextTask.classList.remove('completed');
+      selectedTask.classList.add('completed');
+    }
+    selectedTask.classList.remove('selected');// Take out style from previous task
+    selectedTask.style.backgroundColor = '';
+
+    selectedTask.innerHTML = nextTaskInnerHTML;// Set previous task to next position
+  } else {
+    alert('A tarefa já está na última posição.');
+  }
+});
