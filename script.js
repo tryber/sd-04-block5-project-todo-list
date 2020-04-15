a63e04ac2b43e1d65506ed2de11589eae2376e40
djonata-dias <diasdjonata@gmail.com>
2020-04-15 20:31:43 -0300

    adicionado os botoẽs de remoção de itens da lista quando selecionados, finalizados e todos os itens

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-window.onload = function () {
-  let taskList = document.getElementById("lista-tarefas");
-  let taskText = document.getElementById("texto-tarefa");
-  let createButton = document.getElementById("criar-tarefa");
-  let tasks = document.querySelectorAll('li#lista-tarefas')
-
-  createButton.addEventListener("click", function () {
+window.onload = () => {
+  const taskList = document.getElementById("lista-tarefas");
+  const taskText = document.getElementById("texto-tarefa");
+  const createButton = document.getElementById("criar-tarefa");
+  const allRemoveButton = document.getElementById("apaga-tudo");
+  const doneRemoveButton = document.getElementById("remover-finalizados")
+  const selectedRemoveButton = document.getElementById("remover-selecionado")
+  createButton.addEventListener("click", () => {
     if (taskText.value) {
       const tarefa = document.createElement("li");
       tarefa.innerHTML = taskText.value;
       taskList.appendChild(tarefa);
       taskText.value = "";
-      tarefa.addEventListener("dblclick", () => {tarefa.classList.toggle("marked")});
-      tarefa.addEventListener("click", () => {tarefa.classList.toggle('selected')});
-      
+      tarefa.addEventListener("dblclick", () => {
+        tarefa.classList.toggle("completed");
+      });
+      tarefa.addEventListener("click", () => {
+        tarefa.classList.toggle("selected");
+      });
     }
+  });
+  allRemoveButton.addEventListener("click", () => {
+    const allTasks = document.getElementsByTagName("li");
+    [...allTasks].forEach((element) => {
+      taskList.removeChild(element);
+    });
+  });
+  doneRemoveButton.addEventListener("click", () => {
+    const doneTasks = document.getElementsByClassName("completed");
+    [...doneTasks].forEach((element) => {
+      taskList.removeChild(element);
+    });
+  });
+  selectedRemoveButton.addEventListener("click", () => {
+    const selectedTasks = document.getElementsByClassName("selected");
+    [...selectedTasks].forEach((element) => {
+      taskList.removeChild(element);
+    });
   });
 };
