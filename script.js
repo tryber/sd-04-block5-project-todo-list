83ba6808ace06070f5b29992d79f2e210ab33303
binhus <eng.robsonwilli@gmail.com>
2020-04-16 20:14:33 -0300

    click to select and complete

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ const lista = document.getElementById('lista-tarefas');
 let tarefa = "";
 
 
-
-
 btn.addEventListener('click', function (){
-  const criarTarefa = document.createElement("LI")
+  const criarTarefa = document.createElement("LI");
+  criarTarefa.className = 'item-lista';
   tarefa = document.createTextNode(textoTarefa.value);
   criarTarefa.appendChild(tarefa);
   lista.appendChild(criarTarefa);
+  tarefa = "";
+});
+lista.addEventListener('click', function (event){
+  console.log(event.target);
+  if(event.target.className=='item-lista'){
+    event.target.className = 'item-lista selected';
+  }else if(event.target.className == 'item-lista selected'){
+    event.target.className = 'item-lista completed';
+  }
 });
