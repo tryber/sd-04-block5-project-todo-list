d7771285ac3cd5ef88d835c68baa41ce24876e75
cacobribeiro <cacobribeiro@gmail.com>
2020-04-16 01:01:53 -0300

    fix code climate

--- script.js
+++ script.js
@@ -2,19 +2,17 @@ const criarTarefa = document.getElementById('criar-tarefa');
 const tarefa = document.getElementById('texto-tarefa');
 const ol = document.getElementById('lista-tarefas');
 const deleteTarefas = document.getElementById('apaga-tudo');
-const item = document.getElementsByClassName('item');
 const buttons = document.getElementsByTagName('button');
 
 
 // Cursor em forma de mãozinha
-for (let i = 0; i < buttons.length; i +=1 ) {
+for (let i = 0; i < buttons.length; i += 1) {
   buttons[i].addEventListener('mouseover', function () {
-  event.target.style.cursor = 'pointer';
-});
+    event.target.style.cursor = 'pointer';
+  });
 }
 
 // criar e adicionar li's ao OL.
-criarTarefa.addEventListener('click', addLi);
 function addLi() {
   const li = document.createElement('li');
   li.innerHTML = tarefa.value;
@@ -22,6 +20,8 @@ function addLi() {
   ol.appendChild(li);
   tarefa.value = '';
 }
+criarTarefa.addEventListener('click', addLi);
+
 
 // Deleta tudo de OL. *Apaga todas LI*//
 
@@ -36,11 +36,11 @@ ol.addEventListener('dblclick', function (event) {
   event.target.classList.toggle('completed');
 });
 ol.addEventListener('click', function (event) {
-    const selected = document.querySelectorAll('.selected');
-    for (let i = 0; i < selected.length; i += 1) {
-        selected[i].classList.remove('selected');
-        selected[i].style.backgroundColor = '';
+  const selected = document.querySelectorAll('.selected');
+  for (let i = 0; i < selected.length; i += 1) {
+    selected[i].classList.remove('selected');
+    selected[i].style.backgroundColor = '';
     }
-    event.target.classList.add('selected');
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-})
+  event.target.classList.add('selected');
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+});
