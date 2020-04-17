f58a7d7b029db54a31dffabc5885ec2a5ab63c47
binhus <eng.robsonwilli@gmail.com>
2020-04-16 22:37:47 -0300

    adding erase all and erase completed task's buttons

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 const textoTarefa = document.getElementById('texto-tarefa');
 const btn = document.getElementById('criar-tarefa');
+const btnApagaTudo = document.getElementById('apaga-tudo');
+const btnApagaFinalizados = document.getElementById('remover-finalizados');
 const lista = document.getElementById('lista-tarefas');
-let tarefa = "";
+let tarefa = '';
 
 
-btn.addEventListener('click', function (){
-  const criarTarefa = document.createElement("LI");
+btn.addEventListener('click', function () {
+  const criarTarefa = document.createElement('LI');
   criarTarefa.className = 'item-lista';
   tarefa = document.createTextNode(textoTarefa.value);
   criarTarefa.appendChild(tarefa);
   lista.appendChild(criarTarefa);
-  tarefa = "";
+  tarefa = '';
 });
-lista.addEventListener('click', function (event){
-  console.log(event.target);
-  if(event.target.className=='item-lista'){
+lista.addEventListener('click', function (event) {
+  if (event.target.className === 'item-lista') {
     event.target.className = 'item-lista selected';
-  }else if(event.target.className == 'item-lista selected'){
+  } else if (event.target.className === 'item-lista selected') {
     event.target.className = 'item-lista completed';
+  }
+});
+btnApagaTudo.addEventListener('click', function () {
+  lista.innerHTML = '';
+});
+btnApagaFinalizados.addEventListener('click', function () {
+  const quantosFinalizados = lista.querySelectorAll('.completed').length;
+  for (let i = 0; i < quantosFinalizados; i += 1) {
+    lista.querySelector('.completed').remove('li');
   }
 });
