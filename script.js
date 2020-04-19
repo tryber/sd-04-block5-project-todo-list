4274208e3ca6be8cc00e2e61524ded01d6083177
Kaecio <kaecio@yahoo.com.br>
2020-04-19 20:47:10 -0300

    criando botao apaga toda a lista, criando mao em cima da lista

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 window.onload = function () {
   const text = document.getElementById("texto-tarefa");
   const buttonAdd = document.getElementById("criar-tarefa");
+  const buttonClearAll = document.getElementById("apaga-tudo");
+  // const list = document.createElement("li");
+  const addList = document.querySelector("#lista-tarefas");
 
   function recovery() {}
 
-  document.addEventListener("mouseover", hand);
-
+  //FUNÇÃO MÃOZINHA
   function hand() {
     let x = document.getElementsByClassName("buttonSend");
     for (let i = 0; i < x.length; i++) {
       x[i].style.cursor = "pointer";
     }
   }
+  document.addEventListener("mouseover", hand);
 
+  // BOTÃO ADD ITENS NA LISTA
   buttonAdd.addEventListener("click", function () {
     const addList = document.querySelector("#lista-tarefas");
     const text = document.getElementById("texto-tarefa");
@@ -20,6 +24,7 @@ window.onload = function () {
     console.log(list);
     list.innerHTML = text.value;
     addList.appendChild(list);
+    list.classList.add("list");
     text.value = null;
   });
 
@@ -34,4 +39,17 @@ window.onload = function () {
   // }
 
   // buttonAdd.addEventListener("click",addItem);
+
+  //BOTÃO APAGA TODA A LISTA
+
+  function clearList() {
+    let listClear = document.querySelectorAll(".list");
+    for (let i = 0; i < listClear.length; i++) {
+      addList.removeChild(listClear[i]);
+     }
+  }
+
+  buttonClearAll.addEventListener("click", clearList);
+
+  // let paintList = function panitList() {};
 };
