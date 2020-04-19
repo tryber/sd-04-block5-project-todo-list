e5df62912f99b4c5d7d228a30f6df9925348a21a
Kaecio <kaecio@yahoo.com.br>
2020-04-19 17:04:21 -0300

    criando botao que add otesto na lista

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-let text = document.getElementById("texto-tarefa");
-// let buttonAdd = document.getElementById("criar-tarefa");
+window.onload = function () {
+  const text = document.getElementById("texto-tarefa");
+  const buttonAdd = document.getElementById("criar-tarefa");
 
-function recovery() {}
+  function recovery() {}
 
+  document.addEventListener("mouseover", hand);
 
-document.addEventListener("mouseover", mao);
-
-function mao() {
-  let x = document.getElementsByClassName("buttonSend");
-  for (let i = 0; i < x.length; i++) {
-    x[i].style.cursor = "pointer";
+  function hand() {
+    let x = document.getElementsByClassName("buttonSend");
+    for (let i = 0; i < x.length; i++) {
+      x[i].style.cursor = "pointer";
+    }
   }
-}
+
+  buttonAdd.addEventListener("click", function () {
+    const addList = document.querySelector("#lista-tarefas");
+    const text = document.getElementById("texto-tarefa");
+    const list = document.createElement("li");
+    console.log(list);
+    list.innerHTML = text.value;
+    addList.appendChild(list);
+    text.value = null;
+  });
+
+  // function addItem() {
+  //   const addList = document.querySelector("#lista-tarefas");
+  //   const text = document.getElementById("texto-tarefa");
+  //   const list = document.createElement("li");
+  //   console.log(list);
+  //   list.innerHTML = text.value;
+  //   addList.appendChild(list);
+  //   text.value = "";
+  // }
+
+  // buttonAdd.addEventListener("click",addItem);
+};
