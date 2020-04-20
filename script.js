0adbf04cd3dac6e2e9f8c0745f5e4ddfe48ceed3
Thabata <dornelasthabata@gmail.com>
2020-04-20 17:55:50 -0300

    JS

---
+++ script.js
@@ -0,0 +1,36 @@
+var ol = document.getElementById("lista-tarefas");
+var li;
+var itemId;
+var item;
+
+addTask = function () {
+    if(document.getElementById("texto-tarefa").value != ""){
+    item = document.getElementById("texto-tarefa");
+    itemId = ol.childElementCount;
+    li = createItemEl(item.value, itemId);
+    li.appendChild(createRemoveBtn(itemId));
+    ol.appendChild(li);
+    }
+}
+
+removeTask = function (itemId) {
+    for (i = 0; i < ol.children.length; i++){
+        if (ol.children[i].getAttribute ("index") == itemId) {
+            ol.children[i].remove();
+        }
+    }
+}
+
+createItemEl = function (itemValue, itemId) {
+    let li = document.createElement("li");
+    li.setAttribute("index", itemId);
+    li.appendChild(document.createTextNode(itemValue));
+    return li;
+}
+
+createRemoveBtn = function (itemId) {
+    let btn = document.createElement("button");
+    btn.setAttribute("onclick", "removeTask("+itemId+")");
+    btn.innerHTML = "X";
+    return btn;
+}
