071262586338f9957b05e0b6af494269335f79c0
Kaecio <kaecio@yahoo.com.br>
2020-04-19 23:28:19 -0300

    pintando lista de cinzaa

--- script.js
+++ script.js
@@ -1,55 +1,76 @@
-window.onload = function () {
-  const text = document.getElementById("texto-tarefa");
-  const buttonAdd = document.getElementById("criar-tarefa");
-  const buttonClearAll = document.getElementById("apaga-tudo");
-  // const list = document.createElement("li");
+const text = document.getElementById("texto-tarefa");
+const buttonAdd = document.getElementById("criar-tarefa");
+const buttonClearAll = document.getElementById("apaga-tudo");
+const list = document.createElement("li");
+const addList = document.querySelector("#lista-tarefas");
+// const listCreate = document.querySelectorAll(".list");
+
+const selectItem = document.querySelector("ol");
+
+//FUNÇÃO MÃOZINHA
+function hand() {
+  let x = document.getElementsByClassName("buttonSend");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.cursor = "pointer";
+  }
+}
+document.addEventListener("mouseover", hand);
+
+// BOTÃO ADD ITENS NA LISTA
+buttonAdd.addEventListener("click", function () {
   const addList = document.querySelector("#lista-tarefas");
+  const text = document.getElementById("texto-tarefa");
+  const list = document.createElement("li");
+  console.log(list);
+  list.innerHTML = text.value;
+  addList.appendChild(list);
+  list.classList.add("list");
+  text.value = null;
 
-  function recovery() {}
+  list.addEventListener("click",pintaFundo);
+});
 
-  //FUNÇÃO MÃOZINHA
-  function hand() {
-    let x = document.getElementsByClassName("buttonSend");
-    for (let i = 0; i < x.length; i++) {
-      x[i].style.cursor = "pointer";
-    }
+// function addItem() {
+//   const addList = document.querySelector("#lista-tarefas");
+//   const text = document.getElementById("texto-tarefa");
+//   const list = document.createElement("li");
+//   console.log(list);
+//   list.innerHTML = text.value;
+//   addList.appendChild(list);
+//   text.value = "";
+// }
+
+// buttonAdd.addEventListener("click",addItem);
+
+//BOTÃO APAGA TODA A LISTA
+function clearList() {
+  let listClear = document.querySelectorAll(".list");
+  for (let i = 0; i < listClear.length; i++) {
+    addList.removeChild(listClear[i]);
   }
-  document.addEventListener("mouseover", hand);
+}
+buttonClearAll.addEventListener("click", clearList);
 
-  // BOTÃO ADD ITENS NA LISTA
-  buttonAdd.addEventListener("click", function () {
-    const addList = document.querySelector("#lista-tarefas");
-    const text = document.getElementById("texto-tarefa");
-    const list = document.createElement("li");
-    console.log(list);
-    list.innerHTML = text.value;
-    addList.appendChild(list);
-    list.classList.add("list");
-    text.value = null;
-  });
+//   var paintListn = document.getElementsByClassName("list");
+//  list.addEventListener("click", function () {
+//    for (let i = 0; i < paintListn.length; i++) {
+//     paintListn[i].style.backgroundColor = rgb(180 , 10 , 220 );
+//    console.log(paintListn);
+//    }
+//  })
 
-  // function addItem() {
-  //   const addList = document.querySelector("#lista-tarefas");
-  //   const text = document.getElementById("texto-tarefa");
-  //   const list = document.createElement("li");
-  //   console.log(list);
-  //   list.innerHTML = text.value;
-  //   addList.appendChild(list);
-  //   text.value = "";
-  // }
+// function pintaLista() {
+//   console.log(true);
 
-  // buttonAdd.addEventListener("click",addItem);
+// }
+// selectItem.addEventListener("click", pintaLista);
+// const addList = document.querySelector("#lista-tarefas");
+ const listCreate = document.querySelectorAll(".list");
 
-  //BOTÃO APAGA TODA A LISTA
+// const selectItem = document.querySelector("ol");
 
-  function clearList() {
-    let listClear = document.querySelectorAll(".list");
-    for (let i = 0; i < listClear.length; i++) {
-      addList.removeChild(listClear[i]);
-     }
-  }
+function pintaFundo(event) {
+  const selecionado = event.target;
+  selecionado.classList.add("mystyle");
+  } 
 
-  buttonClearAll.addEventListener("click", clearList);
-
-  // let paintList = function panitList() {};
-};
