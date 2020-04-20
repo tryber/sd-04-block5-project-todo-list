c691560cd8a812e42b433df276bb23613b4eb4c0
Josias Viveiro <josiasviveiro@gmail.com>
2020-04-20 17:28:15 -0300

    js-codeclimate issues fixed

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
+let textTask;
+let taskList;
+let btnCreateTask;
+let btnCleanList;
+let btnRemoveDone;
+
+function clickTask(event) {
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
+}
+
+function completedTask(event) {
+  if (event.target.className === 'completed') {
+    event.target.className = '';
+  } else {
+    event.target.className = 'completed';
+  }
+}
+
+function cleanList() {
+  const listSize = taskList.childNodes.length;
+  for (let i = 0; i < listSize; i += 1) {
+    taskList.removeChild(taskList.lastChild);
+  }
+}
+
+function cleanDone() {
+  const taskDone = document.getElementsByClassName('completed');
+  const taskDoneList = taskDone.length;
+  for (let i = 0; i < taskDoneList; i += 1) {
+    taskDone[0].remove();
+  }
+}
+
+function createTask() {
+  const newTask = document.createElement('li');
+  newTask.innerText = textTask.value;
+  newTask.addEventListener('click', clickTask);
+  newTask.addEventListener('dblclick', completedTask);
+  taskList.appendChild(newTask);
+  textTask.value = '';
+}
+
 window.onload = function () {
-  let textTask = document.getElementById('texto-tarefa');
-  let btnCreateTask = document.getElementById('criar-tarefa');
-  let taskList = document.getElementById('lista-tarefas');
-  let btnCleanList = document.getElementById('apaga-tudo');
-  let btnRemoveDone = document.getElementById('remover-finalizados');
-
-  function clickTask(event) {
-    event.target.style.backgroundColor = 'rgb(128,128,128)';
-  }
-
-  function completedTask(event) {
-    if (event.target.className === 'completed') {
-      event.target.className = '';
-    } else {
-      event.target.className = 'completed';
-    }
-  }
-
-  function cleanList() {
-    let listSize = taskList.childNodes.length;
-    for (let i = 0; i < listSize; i += 1) {
-      taskList.removeChild(taskList.lastChild);
-    }
-  }
-
-  function cleanDone() {
-    let taskDone = document.getElementsByClassName('completed');
-    let taskDoneList = taskDone.length;
-    for (let i = 0; i < taskDoneList; i += 1) {
-        taskDone[0].remove();
-    }
-  }
-
-  function createTask() {
-    let newTask = document.createElement('li');
-    newTask.innerText = textTask.value;
-    newTask.addEventListener('click', clickTask);
-    newTask.addEventListener('dblclick', completedTask);
-    taskList.appendChild(newTask);
-    textTask.value = '';
-  }
+  textTask = document.getElementById('texto-tarefa');
+  btnCreateTask = document.getElementById('criar-tarefa');
+  taskList = document.getElementById('lista-tarefas');
+  btnCleanList = document.getElementById('apaga-tudo');
+  btnRemoveDone = document.getElementById('remover-finalizados');
 
   btnCreateTask.addEventListener('click', createTask);
   btnCleanList.addEventListener('click', cleanList);
