9a93e2f3d1e843f3290240f22142d62421758cc2
lucasokazava <lucasokazava@gmail.com>
2020-04-21 13:32:43 -0300

    requisitos 7 até o 9

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ const criarTarefa = document.getElementById('criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
 const caixaDeTexto = document.getElementById('texto-tarefa');
 
-function addItem () {
+function addItem() {
   const li = document.createElement('li');
   li.innerHTML = caixaDeTexto.value;
   lista.appendChild(li);
-  caixaDeTexto.value = null; 
+  caixaDeTexto.value = null;
+}
+function alterarFundo(event) {
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
+}
+
+function riscado(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  }
+  else {
+    event.target.classList.add('completed');
+  }
 }
 
 criarTarefa.addEventListener('click', addItem);
+lista.addEventListener('click', alterarFundo);
+lista.addEventListener('dblclick', riscado);
