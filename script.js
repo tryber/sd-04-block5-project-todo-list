af71b0f4de48a18bec34bc72f71361a0e4b28f94
yoshidalucas <lucaslimayoshida@gmail.com>
2020-04-21 13:14:57 -0300

    agora vai

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+//  tarefas
 const lista = document.getElementById('lista-tarefas');
+//  verificar se há alguma lista salva anteriormente no local
+//  e se houver, gerar a lista salva
 if (typeof Storage !== 'undefined') {
   lista.innerHTML = localStorage.lista;
 }
-
+//  botoes
 const btnCriar = document.getElementById('criar-tarefa');
 const btnApagaSel = document.getElementById('remover-selecionado');
 const btnApagaFin = document.getElementById('remover-finalizados');
@@ -10,14 +13,15 @@ const btnApagaTudo = document.getElementById('apaga-tudo');
 const btnCima = document.getElementById('mover-cima');
 const btnBaixo = document.getElementById('mover-baixo');
 const btnSalvarTarefas = document.getElementById('salvar-tarefas');
-
+//  eventos relacionados a lista
+//  selecionar tarefa
 lista.addEventListener('click', function () {
   if (document.querySelector('.selected') !== null) {
     document.querySelector('.selected').classList.remove('selected');
   }
   event.target.classList.add('selected');
 });
-
+//  concluir tarefa
 lista.addEventListener('dblclick', function () {
   if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
@@ -25,7 +29,7 @@ lista.addEventListener('dblclick', function () {
     event.target.classList.add('completed');
   }
 });
-
+//  criar tarefa
 btnCriar.addEventListener('click', function () {
   const tarefa = document.getElementById('texto-tarefa');
   const item = document.createElement('li');
@@ -33,12 +37,12 @@ btnCriar.addEventListener('click', function () {
   lista.appendChild(item);
   tarefa.value = '';
 });
-
+//  apagar selecionado
 btnApagaSel.addEventListener('click', () => {
   const tarefaSel = document.querySelector('.selected');
   lista.removeChild(tarefaSel);
 });
-
+//  apagar concluidos
 btnApagaFin.addEventListener('click', () => {
   const tarefas = document.querySelectorAll('li');
   for (let i = 0; i < tarefas.length; i += 1) {
@@ -47,14 +51,14 @@ btnApagaFin.addEventListener('click', () => {
     }
   }
 });
-
+//  apagar tudo
 btnApagaTudo.addEventListener('click', () => {
   const tarefas = document.querySelectorAll('li');
   for (let i = 0; i < tarefas.length; i += 1) {
     lista.removeChild(tarefas[i]);
   }
 });
-
+//  mover tarefa para cima
 btnCima.addEventListener('click', function () {
   const tarefaSel = document.querySelector('.selected');
   const acima = tarefaSel.innerHTML;
@@ -64,7 +68,7 @@ btnCima.addEventListener('click', function () {
   tarefaSel.innerHTML = abaixo;
   tarefaSel.classList.remove('selected');
 });
-
+//  mover tarefa para baixo
 btnBaixo.addEventListener('click', function () {
   const tarefaSel = document.querySelector('.selected');
   const acima = tarefaSel.nextElementSibling.innerHTML;
@@ -74,7 +78,7 @@ btnBaixo.addEventListener('click', function () {
   tarefaSel.innerHTML = acima;
   tarefaSel.classList.remove('selected');
 });
-
+//  salvar lista localmente
 btnSalvarTarefas.addEventListener('click', function () {
   localStorage.lista = document.getElementById('lista-tarefas').innerHTML;
 });
