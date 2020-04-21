136e319dddd0c75308897a713967df50611ed31d
Julianafatsil <julianafatsil@gmail.com>
2020-04-21 12:51:35 -0300

    Criação da função excluir toda lista de tarefas no botão

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const TASKTEXT = document.getElementById('texto-tarefa');
 const BTNCREATETASK = document.getElementById('criar-tarefa');
 const TASKLIST = document.getElementById('lista-tarefas');
+const BTNREMOVELIST =  document.getElementById('apaga-tudo')
 
-function CreateItem() {
+function CreateItem () {
   if (TASKTEXT.value === '') {
     alert('Você precisa digitar alguma tarefa primeiro');
   } else {
@@ -13,17 +14,27 @@ function CreateItem() {
   }
 };
 
+function RemoveList () {
+  let items = document.querySelectorAll('li');
+  for (let i = 0; i < items.length; i += 1) {
+    TASKLIST.removeChild(items[i]);
+  }
+}
+
 window.onload = function () {
-  BTNCREATETASK.addEventListener('click', function() {
+  BTNCREATETASK.addEventListener('click', function () {
     CreateItem();
   });
 
-  TASKLIST.addEventListener('click', function(event) {
+  TASKLIST.addEventListener('click', function (event) {
     event.target.classList.toggle('selected');
   });
 
-  TASKLIST.addEventListener('dblclick', function(event) {
+  TASKLIST.addEventListener('dblclick', function (event) {
     event.target.classList.toggle('completed');
   });
   
+  BTNREMOVELIST.addEventListener('click' , function () {
+    RemoveList();
+  });
 };
