b2a0d5b79f096ff169e4c2322733ea678cfcf96f
yoshidalucas <lucaslimayoshida@gmail.com>
2020-04-21 02:22:07 -0300

    adicionando mouseover para alterar cursor do mouse nos botoes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 window.onload = function () {
-  //  a
+  document.getElementById('criar-tarefa').style.cursor = 'pointer';
+  document.getElementById('remover-selecionado').style.cursor = 'pointer';
+  document.getElementById('remover-finalizados').style.cursor = 'pointer';
+  document.getElementById('apaga-tudo').style.cursor = 'pointer';
+  document.getElementById('mover-cima').style.cursor = 'pointer';
+  document.getElementById('mover-baixo').style.cursor = 'pointer';
+  document.getElementById('salvar-tarefas').style.cursor = 'pointer';
 };
