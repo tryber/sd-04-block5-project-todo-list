5852f936293b49f0c71c3a6355d659a636fcf2e4
Julianafatsil <julianafatsil@gmail.com>
2020-04-21 20:06:17 -0300

    Code refactorings

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const TASKTEXT = document.getElementById('texto-tarefa');
 const BTNCREATETASK = document.getElementById('criar-tarefa');
 const TASKLIST = document.getElementById('lista-tarefas');
-const BTNREMOVELIST = document.getElementById('apaga-tudo')
+const BTNREMOVELIST = document.getElementById('apaga-tudo');
 const BTNREMOVEFINISHEDLIST = document.getElementById('remover-finalizados');
 const BTNREMOVESELECTED = document.getElementById('remover-selecionado');
 const BTNSALVAR = document.getElementById('salvar-tarefas');
@@ -17,27 +17,6 @@ function CreateItem() {
     TASKLIST.appendChild(ITEM);
     TASKTEXT.value = '';
   }
-};
-
-function RemoveList(action) {
-  let items = document.querySelectorAll('li');
-  for (let i = 0; i < items.length; i += 1) {
-    switch (action) {
-      case 2:
-        if (items[i].className === 'completed') {
-          removeChildList(items[i]);
-        }
-        break;
-      case 3:
-        if (items[i].className === 'selected') {
-          removeChildList(items[i]);
-        }
-        break;
-      default:
-        removeChildList(items[i]);
-        break;
-    }
-  }
 }
 
 function removeChildList(item) {
@@ -45,64 +24,72 @@ function removeChildList(item) {
   localStorage.removeItem('itens');
 }
 
-function UpAndDownItemList(isUp) {
-  const ITEMSSELECTED = document.querySelector('.selected')
-  if (ITEMSSELECTED == null) {
-    alert('Você deve selecionar o item que deseja mover para cima!')
-  } else {
-    let auxText = ITEMSSELECTED.innerText;
-    let isCompleted = ITEMSSELECTED.classList.contains('completed');
-
-    let proxItem = (isUp ? ITEMSSELECTED.previousElementSibling : ITEMSSELECTED.nextElementSibling);
-
-    if (proxItem) {
-      ITEMSSELECTED.innerText = proxItem.innerText;
-      ITEMSSELECTED.classList = proxItem.classList;
-      proxItem.innerText = auxText;
-      proxItem.classList = `selected ${(isCompleted ? 'completed' : '')}`;
+function RemoveList(classRemove) {
+  const ITEMS = document.querySelectorAll('li');
+  for (let i = 0; i < ITEMS.length; i += 1) {
+    if ((classRemove === 'remove') ||
+      (ITEMS[i].className.trim() === classRemove)) {
+      removeChildList(ITEMS[i]);
     }
   }
 }
 
-window.onload = function () {
-  BTNCREATETASK.addEventListener('click', function () {
-    CreateItem();
-  });
+function UpAndDownItemList(isUp) {
+  const ITEMSSELECTED = document.querySelector('.selected');
+  if (ITEMSSELECTED === null) {
+    alert('Você deve selecionar o item que deseja mover para cima!');
+  } else {
+    const AUXTEXT = ITEMSSELECTED.innerText;
+    const ISCOMPLETED = ITEMSSELECTED.classList.contains('completed');
 
-  TASKLIST.addEventListener('click', function (event) {
-    event.target.classList.toggle('selected');
-  });
+    const PROXITEM = (isUp ? ITEMSSELECTED.previousElementSibling :
+      ITEMSSELECTED.nextElementSibling);
 
-  TASKLIST.addEventListener('dblclick', function (event) {
-    event.target.classList.toggle('completed');
-  });
-
-  BTNREMOVELIST.addEventListener('click', function () {
-    RemoveList(1);
-  });
-
-  BTNREMOVEFINISHEDLIST.addEventListener('click', function () {
-    RemoveList(2);
-  });
-
-  BTNREMOVESELECTED.addEventListener('click', function () {
-    RemoveList(3);
-  });
-
-  BTNSALVAR.addEventListener('click', function () {
-    localStorage.setItem('itens', TASKLIST.innerHTML);
-  });
-
-  if (localStorage !== null) {
-    TASKLIST.innerHTML = localStorage.getItem('itens');
+    if (PROXITEM) {
+      ITEMSSELECTED.innerText = PROXITEM.innerText;
+      ITEMSSELECTED.classList = PROXITEM.classList;
+      PROXITEM.innerText = AUXTEXT;
+      PROXITEM.classList = `selected ${(ISCOMPLETED ? 'completed' : '')}`;
+    }
   }
+}
 
-  BTNUP.addEventListener('click', function () {
-    UpAndDownItemList(true);
-  });
+TASKLIST.addEventListener('click', function (event) {
+  event.target.classList.toggle('selected');
+});
 
-  BTNDOWN.addEventListener('click', function () {
-    UpAndDownItemList(false);
-  });
+TASKLIST.addEventListener('dblclick', function (event) {
+  event.target.classList.toggle('completed');
+});
 
-};
+BTNUP.addEventListener('click', function () {
+  UpAndDownItemList(true);
+});
+
+BTNDOWN.addEventListener('click', function () {
+  UpAndDownItemList(false);
+});
+
+BTNSALVAR.addEventListener('click', function () {
+  localStorage.setItem('itens', TASKLIST.innerHTML);
+});
+
+if (localStorage !== null) {
+  TASKLIST.innerHTML = localStorage.getItem('itens');
+}
+
+BTNREMOVELIST.addEventListener('click', function () {
+  RemoveList('remove');
+});
+
+BTNREMOVEFINISHEDLIST.addEventListener('click', function () {
+  RemoveList('completed');
+});
+
+BTNREMOVESELECTED.addEventListener('click', function () {
+  RemoveList('selected');
+});
+
+BTNCREATETASK.addEventListener('click', function () {
+  CreateItem();
+});
