f8396ca34e33e8868af9df7af1c727f934d18a84
yoshidalucas <lucaslimayoshida@gmail.com>
2020-04-21 12:04:58 -0300

    adicionndo requisitos 5 ao 9

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 const lista = document.getElementById('lista-tarefas');
 const criar = document.getElementById('criar-tarefa');
-document.getElementById('criar-tarefa').style.cursor = 'pointer';
-document.getElementById('remover-selecionado').style.cursor = 'pointer';
-document.getElementById('remover-finalizados').style.cursor = 'pointer';
-document.getElementById('apaga-tudo').style.cursor = 'pointer';
-document.getElementById('mover-cima').style.cursor = 'pointer';
-document.getElementById('mover-baixo').style.cursor = 'pointer';
-document.getElementById('salvar-tarefas').style.cursor = 'pointer';
-document.getElementById('lista-tarefas').style.cursor = 'pointer';
-
-
-
 criar.addEventListener('click', function () {
   const tarefa = document.getElementById('texto-tarefa');
   const item = document.createElement('li');
   item.innerHTML = tarefa.value;
   lista.appendChild(item);
-  tarefa.value='';
+  tarefa.value = '';
 });
-
+lista.addEventListener('click', function () {
+  if (document.querySelector('.selected') !== null) {
+    document.querySelector('.selected').style.backgroundColor = 'rgb(29,29,29)';
+    document.querySelector('.selected').classList.remove('selected');
+  }
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
+  event.target.classList.add('selected');
+});
