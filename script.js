60227163985a38813d70ccf34263243b4994ed05
yoshidalucas <lucaslimayoshida@gmail.com>
2020-04-21 01:39:55 -0300

    adicionando mouseover para alterar cursor do mouse nos botoes

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 window.onload = function () {
-document.querySelector('.lista').addEventListener('mouseover', function () { document.querySelector('.lista').style.cursor = 'pointer'; });
+  //  a
 };
