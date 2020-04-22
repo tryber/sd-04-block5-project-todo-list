612b4a4b2af85c28a0fb6f74be2dfe68773f3563
Leandro <lekaoru@gmail.com>
2020-04-22 10:32:09 -0400

    Corrigindo erros

--- script.js
+++ script.js
@@ -1,92 +1,91 @@
 window.onload = function () {
-    let buttonAdd = document.getElementsByClassName("adicionando-tarefa")[0];
-    let buttonClear = document.getElementsByClassName("rmv-tarefas")[0];
-    let buttonFinalizados = document.getElementsByClassName("rmv-executados")[0];
-    let buttonSalve = document.getElementsByClassName("save")[0];
-    let buttonRemove = document.getElementsByClassName("rmv-selecionado")[0]
-    let buttonDown = document.getElementsByClassName("move-baixo")[0];
-    let buttonUp = document.getElementsByClassName("move-cima")[0];
-    let lista = document.getElementsByClassName("lista-ordenada")[0];
-    let input1 = document.getElementsByClassName("tarefa")[0];
-
-
+    const buttonAdd = document.getElementsByClassName('adicionando-tarefa')[0];
+    const buttonClear = document.getElementsByClassName('rmv-tarefas')[0];
+    const buttonFinalizados = document.getElementsByClassName('rmv-executados')[0];
+    const buttonSalve = document.getElementsByClassName('save')[0];
+    const buttonRemove = document.getElementsByClassName('rmv-selecionado')[0]
+    const buttonDown = document.getElementsByClassName('move-baixo')[0];
+    const buttonUp = document.getElementsByClassName('move-cima')[0];
+    const lista = document.getElementsByClassName('lista-ordenada')[0];
+    const input1 = document.getElementsByClassName('tarefa')[0];
 
     function listed() {
-        let item1 = document.createElement("li");
+        let item1 = document.createElement('li');
         lista.appendChild(item1);
         item1.innerHTML = input1.value;
-        item1.classList.add("li-criada");
+        item1.classList.add('li-criada');
         backgroundGrey(item1);
         riskingOrNot(item1);
-        input1.value = "";
+        input1.value = '';
     }
-    buttonAdd.addEventListener("click", listed)
+    buttonAdd.addEventListener('click', listed);
 
     function backgroundGrey(element) {
-        element.addEventListener("click", function () {
-            if (document.getElementsByClassName("selecionado")[0]) {
-                document.getElementsByClassName("selecionado")[0].classList.remove("selecionado");
-                element.classList.add("selecionado");
+        element.addEventListener('click', function () {
+            if (document.getElementsByClassName('selecionado')[0]) {
+                document.getElementsByClassName('selecionado')[0].classList.remove('selecionado');
+                element.classList.add('selecionado');
             } else {
-                element.classList.add("selecionado");
+                element.classList.add('selecionado');
             }
         });
     }
+
     function riskingOrNot(element) {
-        element.addEventListener("dblclick", function () {
-            if (element.classList.contains("completed")) {
-                element.classList.remove("completed");
+        element.addEventListener('dblclick', function () {
+            if (element.classList.contains('completed')) {
+                element.classList.remove('completed');
             } else {
-                element.classList.add("completed");
+                element.classList.add('completed');
             }
-        })
+        });
     }
 
-    buttonClear.addEventListener("click", function () {
+    buttonClear.addEventListener('click', function () {
         while (lista.lastElementChild) {
             lista.removeChild(lista.lastElementChild);
         }
-    })
+    });
 
-    buttonFinalizados.addEventListener("click", function () {
-        document.querySelectorAll(".completed").forEach(function (element) {
+    buttonFinalizados.addEventListener('click', function () {
+        document.querySelectorAll('.completed').forEach(function (element) {
             element.remove();
-        })
-    })
+        });
+    });
 
-    buttonSalve.addEventListener("click", function () {
-        localStorage.setItem("Lista de Tarefas", lista.innerHTML);
-    })
+    buttonSalve.addEventListener('click', function () {
+        localStorage.setItem('Lista de Tarefas', lista.innerHTML);
+    });
 
-    if (window.localStorage.getItem("Lista de Tarefas")) {
-        lista.innerHTML = window.localStorage.getItem("Lista de Tarefas");
+    if (window.localStorage.getItem('Lista de Tarefas')) {
+        lista.innerHTML = window.localStorage.getItem('Lista de Tarefas');
     }
 
     function deleteSelected() {
-        document.querySelectorAll(".selecionado").forEach(function (element) {
+        document.querySelectorAll('.selecionado').forEach(function (element) {
             element.remove()
-        })
+        });
     }
-    buttonRemove.addEventListener("click", deleteSelected);
+    buttonRemove.addEventListener('click', deleteSelected);
 
 
 
     function moveUp() {
-        selecionado = document.querySelectorAll(".selecionado");
+        selecionado = document.querySelectorAll('.selecionado');
         for (let i = 0; i < selecionado.length; i++) {
             lista.insertBefore(selecionado[i], selecionado[i].previousSibling);
         }
     }
-    buttonUp.addEventListener("click", moveUp);
+    buttonUp.addEventListener('click', moveUp);
 
     function moveDown() {
-        let selecionado = document.querySelectorAll(".selecionado")[0];
+        let selecionado = document.querySelectorAll('.selecionado')[0];
         if (selecionado === lista.lastChild) {
-            selecionado = lista.insertBefore(selecionado, document.querySelectorAll("li")[0]);
+            selecionado = lista.insertBefore(selecionado, document.querySelectorAll('li')[0]);
         } else {
             lista.insertBefore(selecionado, selecionado.nextSibling.nextSibling);
         }
     }
-    buttonDown.addEventListener("click", moveDown);
+    buttonDown.addEventListener('click', moveDown);
 
-} 
+}
