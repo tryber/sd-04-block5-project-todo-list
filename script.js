62ed56c91e3f0e1efe8e743540eb71d72ac2d308
Douglas <douglaza@gmail.com>
2020-05-02 01:37:04 -0300

    fixing two more requirements

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 const can = document.getElementById('lista-tarefas');
+const linhas = can.childNodes;
+window.onload = restauraLista();
 
-function incluirItem() { // https://codare.aurelio.net/2006/12/20/javascript-construindo-listas-dinamicamente/
+function incluirItem() { // https://bit.ly/3cZqVpE
   const tarefaTexto = document.getElementById('texto-tarefa');
   const li = document.createElement('li');
-  // li.className = 'cible';
-  // li.innerHTML = tarefaTexto.value.toUpperCase();
   li.innerHTML = tarefaTexto.value;
   can.appendChild(li);
   document.getElementById('texto-tarefa').value = '';
 }
 
 function apagarLista() {
-  const linhas = can.childNodes;
+  // const linhas = can.childNodes;
   for (let i = linhas.length - 1; i >= 0; i -= 1) {
     if (linhas[i].tagName === 'LI') {
       can.removeChild(linhas[i]);
+      localStorage.removeItem(`Tarefa${i}`);
     }
   }
 }
 
 function excluirItem() {
-  const linhas = can.childNodes;
+  // const linhas = can.childNodes;
   for (let i = linhas.length - 1; i >= 0; i -= 1) {
     if (linhas[i].className === 'completed') {
       can.removeChild(linhas[i]);
+    }
+  }
+}
+
+function salvarTarefas() {
+  for (let i = 1; i < linhas.length; i += 1) {
+    localStorage.setItem(`Tarefa${i}`, linhas[i].innerHTML);
+  }
+}
+
+function restauraLista() {
+  for (let i = 1; i <= localStorage.length; i += 1) {
+    const li = document.createElement('li');
+    if (`Tarefa${i}`) {
+      li.innerHTML = localStorage.getItem(`Tarefa${i}`);
+      can.appendChild(li);
     }
   }
 }
