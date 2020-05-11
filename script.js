2c65db0503ebf30f5a48aa046ef9fe8e2504f6c3
Lucas Staroscky <staroscky60@gmail.com>
2020-05-10 23:56:10 -0300

    alterações requisitos

--- script.js
+++ script.js
@@ -6,69 +6,60 @@ const btnRemoveCompleted = document.getElementById("remover-finalizados")
 const btnsalvarTarefa = document.getElementById("salvar-tarefas")
 
 
-// Funcao parar criar uma nova tarefa
+//  funcao criar uma nova tarefa
 function createTask() {
-let insertList = document.getElementsByTagName("ol")[0]
-let valor = elTexto.value;
-let tarefa = document.createElement("li")
-tarefa.innerHTML = valor
-tarefa.addEventListener("click", checkItem)
-tarefa.addEventListener("dblclick", checkComplete)
-insertList.appendChild(tarefa)
-elTexto.value = "";
+	let insertList = document.getElementsByTagName("ol")[0]
+	let valor = elTexto.value;
+	let tarefa = document.createElement("li")
+	tarefa.innerHTML = valor
+	tarefa.addEventListener("click", checkItem)
+	tarefa.addEventListener("dblclick", checkComplete)
+	insertList.appendChild(tarefa)
+	elTexto.value = "";
 }
+
+// funcao selecionar item e alterar cor de fundo 
+function checkItem(event) {
+	let item = event.target;
+	let isActive = item.classList.contains('active');
+	if (isActive === false) {
+		item.classList.add("active")
+	} else {
+		item.classList.remove("active")
+	}
+}
+
+// funcao riscar tarefa completa
+function checkComplete(event) {
+	let item = event.target;
+	let isComplete = item.classList.contains("completed")
+	if (isComplete === false) {
+		item.classList.add("completed")
+	} else {
+		item.classList.remove("completed")
+	}
+}
+
+// funcao para limpar todos os itens
+function clearAll(event) {
+	let item = document.getElementsByTagName("ol")
+	for (let i = 0; i < item.length; i++) {
+		item[i].innerHTML = "";
+	}
+}
+
+// funcao remover apenas itens marcados como completo
+function removeCompleted(event) {
+	let list = document.getElementsByTagName("li")
+	for (let i = 0; i < list.length; i++) {
+		if (list[i].className == "completed") {
+			list[i].remove("li")
+		}
+	}
+}
+
 
 btnTarefa.addEventListener("click", createTask); //cria uma nova tarefa
 btnClear.addEventListener("click", clearAll); //limpa os itens da lista
 btnRemoveCompleted.addEventListener("click", removeCompleted); //remove finalizados
 btnsalvarTarefa.addEventListener("click", saveTasks); //salva as tarefas no localstorage
-
-
-function checkItem(event) {
-let item = event.target;
-let isActive = item.classList.contains('active');
-if (isActive === false) {
-item.classList.add("active")
-} else {
-item.classList.remove("active")
-}
-}
-
-
-function checkComplete(event) {
-let item = event.target;
-let isComplete = item.classList.contains("completed")
-if (isComplete === false) {
-item.classList.add("completed")
-} else {
-item.classList.remove("completed")
-}
-}
-
-function clearAll(event) {
-let item = document.getElementsByTagName("ol")
-for (let i = 0; i < item.length; i++) {
-item[i].innerHTML = "";
-}
-}
-
-function removeCompleted(event) {
-let list = document.getElementsByTagName("li")
-for (let i = 0; i < list.length; i++) {
-if (list[i].className == "completed") {
-    list[i].remove("li")
-}
-}
-}
-
-function saveTasks(event) {
-var conteudo = document.querySelectorAll(".order-list li").map(function () {
-return {
-    classes: this.className,
-    html: this.innerHTML
-}
-}).get();
-console.log(conteude)
-localStorage.setItem('tasks', JSON.stringify(conteudo));
-}
-
