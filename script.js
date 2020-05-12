2baa904f6f1cd4f1315e3faa4df92d3c999aa2f8
Lucas Staroscky <staroscky60@gmail.com>
2020-05-12 01:51:25 -0300

    codeclimate

--- script.js
+++ script.js
@@ -1,63 +1,66 @@
 const elTexto = document.getElementById('texto-tarefa');
 const btnTarefa = document.getElementById('criar-tarefa');
 const btnClear = document.getElementById('apaga-tudo');
-// const btnRemoveCompleted = document.getElementById('remover-finalizados');
-// const btnsalvarTarefa = document.getElementById('salvar-tarefas');
+const btnRemoveCompleted = document.getElementById('remover-finalizados');
+const backgroundItem = document.getElementById("lista-tarefas");
+const checkCompletedItem = document.querySelectorAll("li");
 
+// >>>>>>>>>>>>
+const handleDblClick = () => {
+ const item = event.target;
+ const isComplete = item.classList.contains('completed');
+ if (isComplete === false) {
+  item.classList.add('completed');
+ } else {
+  item.classList.remove('completed');
+ }
+};
+// <<<<<<<<<<<<<<
 
-// funcao selecionar item e alterar cor de fundo
-function checkItem(event) {
-  const item = event.target;
-  const isActive = item.classList.contains('active');
-  if (isActive === false) {
-    item.classList.add('active');
-  } else {
-    item.classList.remove('active');
+btnTarefa.addEventListener('click', () => {
+ const insertList = document.getElementsByTagName('ol')[0];
+ const valor = elTexto.value;
+ const tarefa = document.createElement('li');
+ // >>>>>>
+ tarefa.addEventListener(`dblclick`, handleDblClick);
+ // <<<<<<
+ tarefa.innerHTML = valor;
+ insertList.appendChild(tarefa);
+ elTexto.value = '';
+});
+
+backgroundItem.addEventListener("click", (event) => {
+ const item = event.target;
+ const isActive = item.classList.contains('active');
+ if (isActive === false) {
+  item.classList.add('active');
+ } else {
+  item.classList.remove('active');
+ }
+})
+
+// checkCompletedItem.addEventListener('dblclick', (event) => {
+//    const item = event.target;
+//    const isComplete = item.classList.contains('completed');
+//    if (isComplete === false) {
+//     item.classList.add('completed');
+//    } else {
+//     item.classList.remove('completed');
+//    }
+//   });
+
+btnClear.addEventListener('click', () => {
+ const item = document.getElementsByTagName('ol');
+ for (let i = 0; i < item.length; i += 1) {
+  item[i].innerHTML = '';
+ }
+});
+
+btnRemoveCompleted.addEventListener("click", () => {
+ const list = document.getElementsByTagName('li')
+ for (let i = 0; i < list.length; i += 1) {
+  if (list[i].className == 'completed') {
+   list[i].remove('li')
   }
-}
-
-// funcao riscar tarefa completa
-function checkComplete(event) {
-  const item = event.target;
-  const isComplete = item.classList.contains('completed');
-  if (isComplete === false) {
-    item.classList.add('completed');
-  } else {
-    item.classList.remove('completed');
-  }
-}
-
-// funcao para limpar todos os itens
-function clearAll() {
-  const item = document.getElementsByTagName('ol');
-  for (let i = 0; i < item.length; i += 1) {
-    item[i].innerHTML = '';
-  }
-}
-
-// funcao remover apenas itens marcados como completo
-// function removeCompleted(event) {
-//   const list = document.getElementsByTagName('li')
-//   for (let i = 0; i < list.length; i+=1) {
-//     if (list[i].className == 'completed') {
-//       list[i].remove('li')
-//     }
-//   }
-// }
-
-//  funcao criar uma nova tarefa
-function createTask() {
-  const insertList = document.getElementsByTagName('ol')[0];
-  const valor = elTexto.value;
-  const tarefa = document.createElement('li');
-  tarefa.innerHTML = valor;
-  tarefa.addEventListener('click', checkItem);
-  tarefa.addEventListener('dblclick', checkComplete);
-  insertList.appendChild(tarefa);
-  elTexto.value = '';
-}
-
-btnTarefa.addEventListener('click', createTask); // cria uma nova tarefa
-btnClear.addEventListener('click', clearAll); // limpa os itens da lista
-// btnRemoveCompleted.addEventListener('click', removeCompleted); // remove finalizados
-// btnsalvarTarefa.addEventListener('click', saveTasks); // salva as tarefas no localstorage
+ }
+})
