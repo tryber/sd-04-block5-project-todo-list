e93da7aedd20f239a413619aa51ee9cf609aa33a
Lucas Staroscky <staroscky60@gmail.com>
2020-05-12 12:55:49 -0300

    codeclimate

--- script.js
+++ script.js
@@ -2,33 +2,23 @@ const elTexto = document.getElementById('texto-tarefa');
 const btnTarefa = document.getElementById('criar-tarefa');
 const btnClear = document.getElementById('apaga-tudo');
 const btnRemoveCompleted = document.getElementById('remover-finalizados');
+const btnRemoveSelected = document.getElementById("remover-selecionado")
 const backgroundItem = document.getElementById("lista-tarefas");
 const checkCompletedItem = document.querySelectorAll("li");
+const button =  document.querySelectorAll("button")
 
-// >>>>>>>>>>>>
-const handleDblClick = () => {
- const item = event.target;
- const isComplete = item.classList.contains('completed');
- if (isComplete === false) {
-  item.classList.add('completed');
- } else {
-  item.classList.remove('completed');
- }
-};
-// <<<<<<<<<<<<<<
 
+// criar tarefa
 btnTarefa.addEventListener('click', () => {
  const insertList = document.getElementsByTagName('ol')[0];
  const valor = elTexto.value;
  const tarefa = document.createElement('li');
- // >>>>>>
- tarefa.addEventListener(`dblclick`, handleDblClick);
- // <<<<<<
  tarefa.innerHTML = valor;
  insertList.appendChild(tarefa);
  elTexto.value = '';
 });
 
+// alterar fundo pra cinza 
 backgroundItem.addEventListener("click", (event) => {
  const item = event.target;
  const isActive = item.classList.contains('active');
@@ -37,7 +27,50 @@ backgroundItem.addEventListener("click", (event) => {
  } else {
   item.classList.remove('active');
  }
-})
+});
+
+// cursos pointer
+for (let i = 0; i < button.length; i += 1) {
+ button[i].style.cursor = 'pointer';
+}
+
+// riscar tarefa completa
+const check = document.getElementById("lista-tarefas")
+check.addEventListener(`dblclick`, (event) => {
+ const item = event.target;
+ const isComplete = item.classList.contains('completed');
+ if (isComplete === false) {
+  item.classList.add('completed');
+ } else {
+  item.classList.remove('completed');
+ }
+});
+
+// limpar itens
+btnClear.addEventListener('click', () => {
+ const item = document.getElementsByTagName('ol');
+ for (let i = 0; i < item.length; i += 1) {
+  item[i].innerHTML = '';
+ }
+});
+
+// remover tarefas completas
+btnRemoveCompleted.addEventListener("click", () => {
+ const list = document.querySelectorAll('.completed')
+ for (let i = 0; i < list.length; i += 1) {
+   list[i].parentNode.removeChild(list[i])
+  }
+ });
+
+ // remover selecionado
+ btnRemoveSelected.addEventListener("click", (event) => {
+    const a = document.querySelector(".active")
+    a.parentNode.removeChild(a)
+   
+
+ })
+
+
 
 // checkCompletedItem.addEventListener('dblclick', (event) => {
 //    const item = event.target;
@@ -48,19 +81,4 @@ backgroundItem.addEventListener("click", (event) => {
 //     item.classList.remove('completed');
 //    }
 //   });
-
-btnClear.addEventListener('click', () => {
- const item = document.getElementsByTagName('ol');
- for (let i = 0; i < item.length; i += 1) {
-  item[i].innerHTML = '';
- }
-});
-
-btnRemoveCompleted.addEventListener("click", () => {
- const list = document.getElementsByTagName('li')
- for (let i = 0; i < list.length; i += 1) {
-  if (list[i].className == 'completed') {
-   list[i].remove('li')
-  }
- }
-})
+// 
